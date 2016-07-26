b319e723979961b1182a2868f68f458390e814f5
jiangtian <jtli.ames2013@gmail.com>
2016-07-26 09:02:06 -0700

    more checkin

--- binary_tree_maximum_path_sum.cpp
+++ binary_tree_maximum_path_sum.cpp
@@ -1,3 +1,21 @@
+124. Binary Tree Maximum Path Sum  QuestionEditorial Solution  My Submissions
+Total Accepted: 69809 Total Submissions: 292376 Difficulty: Hard
+Given a binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path does not need to go through the root.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+
+Hide Company Tags Microsoft
+Hide Tags Tree Depth-first Search
+Hide Similar Problems (E) Path Sum (M) Sum Root to Leaf Numbers
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
