815acb0c56a61075baa9c3971a250b3292cdcfdb
jtliames2013 <jtli.ames2013@gmail.com>
2017-04-18 20:53:23 -0700

    more checkin

--- cpp/restore_ip_addresses.cpp
+++ cpp/restore_ip_addresses.cpp
@@ -6,170 +6,39 @@ For example:
 Given "25525511135",
 
 return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <unordered_set>
-#include <map>
-#include <limits.h>
-
-using namespace std;
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
- };
-
-/**
- * Definition for undirected graph.
- * */
-struct UndirectedGraphNode {
-    int label;
-    vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraphNode(int x) : label(x) {};
-};
-
-/**
- * Definition for binary tree with next pointer.
- */
-struct TreeLinkNode {
-  int val;
-  TreeLinkNode *left, *right, *next;
-  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
 
 class Solution {
 public:
-	bool isValidIpPart(string &s, int start, int n)
-	{
-		if (n <= 0 || n > 3 || start+n>s.size()) return false;
-		if (n >= 2 && s[start]=='0') return false;
-
-		return atoi(s.substr(start, n).c_str()) <= 255;
-	}
-
-	vector<string> restore(string &s, int npart, int start, int n)
-	{
-		vector<string> res;
-		if (npart==1)
-		{
-			if (isValidIpPart(s, start, n))
-			{
-				res.push_back(s.substr(start,n));
-			}
-			return res;
-		}
-
-		if (isValidIpPart(s, start, 1))
-		{
-			vector<string> v = restore(s, npart-1, start+1, n-1);
-			if (v.size() > 0)
-			{
-				for (unsigned int i=0; i<v.size(); i++)
-				{
-					string tmp=s.substr(start,1);
-					tmp += ".";
-					tmp += v[i];
-					res.push_back(tmp);
-				}
-			}
-		}
-
-		if (isValidIpPart(s, start, 2))
-		{
-			vector<string> v = restore(s, npart-1, start+2, n-2);
-			if (v.size() > 0)
-			{
-				for (unsigned int i=0; i<v.size(); i++)
-				{
-					string tmp=s.substr(start,2);
-					tmp += ".";
-					tmp += v[i];
-					res.push_back(tmp);
-				}
-			}
-		}
-
-		if (isValidIpPart(s, start, 3))
-		{
-			vector<string> v = restore(s, npart-1, start+3, n-3);
-			if (v.size() > 0)
-			{
-				for (unsigned int i=0; i<v.size(); i++)
-				{
-					string tmp=s.substr(start,3);
-					tmp += ".";
-					tmp += v[i];
-					res.push_back(tmp);
-				}
-			}
-		}
-		return res;
-	}
-
-    vector<string> restoreIpAddresses(string s) {
-    	return restore(s, 4, 0, s.size());
-    }
-};
-
-2. Don't return vector of string
-
-class Solution {
-public:
-    void getIp(vector<string>& res, string& ip, string& s, int index, int numAddr) {
+    void dfs(vector<string>& res, string& s, string& ip, int start, int numAddr) {
         if (numAddr==4) {
-            if (index==s.size()) {
+            if (start==s.size()) {
                 res.push_back(ip);
-                return;
-            } else {
-                return;
             }
-        }
+            return;
+        }    
         
-        for (int i=index; i<=index+2 && i<s.size(); i++) {
-            string str=s.substr(index, i-index+1);
-            int num=atoi(str.c_str());
-            if (num>=0 && num<=255 && (i==index || s[index]!='0')){
+        for (int i=start; i<s.size() && i<=start+2; i++) {
+            int num=0;
+            for (int j=start; j<=i; j++) {
+                num*=10;
+                num+=s[j]-'0';
+            }
+            if (num<=255 && (i==start||s[start]!='0')) {
                 int len=ip.size();
-                if (len>0) {
-                    ip.push_back('.');
-                }
-                ip+=str;
-                getIp(res, ip, s, i+1, numAddr+1);
-                ip.erase(len, ip.size()-len);
+                if (ip!="") ip+=".";
+                ip+=s.substr(start, i-start+1);
+                dfs(res, s, ip, i+1, numAddr+1);
+                ip.erase(len);
             }
         }
-    }    
-
+    }
+    
     vector<string> restoreIpAddresses(string s) {
         vector<string> res;
         string ip;
-        getIp(res, ip, s, 0, 0);        
-
+        dfs(res, s, ip, 0, 0);
+        
         return res;
     }
 };
-
-int main()
-{
-	return 0;
-}
 
--- cpp/word_break_II.cpp
+++ cpp/word_break_II.cpp
@@ -12,157 +12,94 @@ A solution is ["cats and dog", "cat sand dog"].
 Hide Company Tags Dropbox Google Uber Snapchat Twitter
 Hide Tags Dynamic Programming Backtracking
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <list>
-#include <unordered_set>
-#include <map>
-#include <algorithm>
-#include <limits.h>
-#include <math.h>
-
-using namespace std;
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
- };
-
-/**
- * Definition for undirected graph.
- * */
-struct UndirectedGraphNode {
-    int label;
-    vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraphNode(int x) : label(x) {};
-};
-
-/**
- * Definition for binary tree with next pointer.
- */
-struct TreeLinkNode {
-  int val;
-  TreeLinkNode *left, *right, *next;
-  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
-
-/**
- * Definition for an interval.
-*/
- struct Interval {
-      int start;
-      int end;
-      Interval() : start(0), end(0) {}
-      Interval(int s, int e) : start(s), end(e) {}
- };
-
-1. Use the length of res to set canBreak array.
-
- class Solution {
- public:
-	 void getWordBreak(string& s, unordered_set<string>& wordDict, int start, vector<string>& res, vector<string>& words, vector<bool>& canBreak) {
-		 if (start==s.size()) {
-			 string result;
-			 for (int i=0; i<words.size(); i++) {
-				 result.append(words[i]);
-				 if (i<words.size()-1) {
-					 result.append(" ");
-				 }
-			 }
-
-			 res.push_back(result);
-			 return;
-		 }
-
-		 for (int i=start; i<s.size(); i++) {
-			 string w=s.substr(start, i-start+1);
-			 if (wordDict.find(w)!=wordDict.end() && canBreak[i+1]==true) {
-				 words.push_back(w);
-				 int len=res.size();
-				 getWordBreak(s, wordDict, i+1, res, words, canBreak);
-				 // If the res size doesn't change, then it can't break
-				 if (res.size()==len) {
-					 canBreak[i+1]=false;
-				 }
-
-				 words.pop_back();
-			 }
-		 }
-	 }
-
-     vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
-    	 vector<string> res;
-    	 vector<string> words;
-    	 vector<bool> canBreak(s.size()+1, true);
-
-    	 getWordBreak(s, wordDict, 0, res, words, canBreak);
-
-    	 return res;
-     }
- };
-
-2. recursion return boolean. Note if one recursion return true, it should return true in the end but still need to continue all recursion.
 class Solution {
 public:
-    bool findBreak(string& s, unordered_set<string>& wordDict, vector<bool>& canBreak, vector<string>& res, vector<string>& output, int size, int start) {
-        if (start>=size) {
-            string str;
-            for (int i=0; i<output.size(); i++) {
-                if (i==0) str+=output[i];
-                else str+=" "+output[i];
-            }
-            res.push_back(str);
+    string convert(vector<string>& output) {
+        string res;
+        for (int i=0; i<output.size(); i++) {
+            if (i>0) res+=" ";
+            res+=output[i];
+        }
+        return res;
+    }
+    
+    bool wordBreak(vector<string>& res, string& s, unordered_set<string>& dict, vector<bool>& canBreak, vector<string>& output, int start) {
+        if (start==s.size()) {
+            res.push_back(convert(output));
             return true;
         }
         
-        bool canbreak=false;
-        for (int i=start; i<size; i++) {
-            string str=s.substr(start, i-start+1);
-            if (wordDict.find(str)!=wordDict.end() && canBreak[i+1]==true) {
+        bool b=false;
+        for (int i=start; i<s.size(); i++) {
+            if (canBreak[i+1]) {
+                string str=s.substr(start, i-start+1);
                 output.push_back(str);
-                if (findBreak(s, wordDict, canBreak, res, output, size, i+1)==false) canBreak[i+1]=false;
-                else canbreak=true;
+                if (dict.find(str)!=dict.end()) {
+                    if (wordBreak(res, s, dict, canBreak, output, i+1)) {
+                        b=true;
+                    } else {
+                        canBreak[i+1]=false;
+                    }
+                }
                 output.pop_back();
-            }
+            }        
         }
-        
-        return canbreak;
+        return b;
     }
     
-    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string> res;
+        int n=s.size();
+        if (n==0) return res;
+        unordered_set<string> dict;
+        for (auto w:wordDict) dict.insert(w);
         vector<string> output;
-        int size=s.size();
-        if (size==0) return res;
-        vector<bool> canBreak(size+1, true);
+        vector<bool> canBreak(n+1, true);
         
-        findBreak(s, wordDict, canBreak, res, output, size, 0);
+        wordBreak(res, s, dict, canBreak, output, 0);
         
         return res;
     }
 };
 
-int main()
-{
-	return 0;
-}
+2. Memoization
+class Solution {
+public:
+    vector<string> wordBreak(string& s, unordered_set<string>& dict, unordered_map<string, vector<string>>& mp, int start) {
+        vector<string> res;
+        if (start==s.size()) {
+            res.push_back("");
+            return res;
+        }
+        
+        for (int i=start; i<s.size(); i++) {
+            string str=s.substr(start, i-start+1);
+            if (dict.find(str)!=dict.end()) {
+                vector<string> subres;
+                if (mp.find(s.substr(i+1))!=mp.end()) {
+                    subres=mp[s.substr(i+1)];
+                } else {
+                    subres=wordBreak(s, dict, mp, i+1);
+                }        
+                for (auto words:subres) {
+                    if (words=="") res.push_back(str);
+                    else res.push_back(str+" "+words);
+                }
+            }
+        }
+        mp[s.substr(start)]=res;
+        return res;
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> res;
+        int n=s.size();
+        if (n==0) return res;
+        unordered_set<string> dict;
+        for (auto w:wordDict) dict.insert(w);
+        unordered_map<string, vector<string>> mp;
+        
+        return wordBreak(s, dict, mp, 0);
+    }
+};
 
