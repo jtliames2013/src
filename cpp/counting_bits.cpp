4acf1883ed995736616ac6090b5be9c9803958cb
jtliames2013 <jtli.ames2013@gmail.com>
2017-04-25 09:33:05 -0700

    more checkin

--- cpp/counting_bits.cpp
+++ cpp/counting_bits.cpp
@@ -1,10 +1,37 @@
+338. Counting Bits Add to List
+DescriptionHintsSubmissionsSolutions
+Total Accepted: 72823
+Total Submissions: 120579
+Difficulty: Medium
+Contributor: LeetCode
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+Credits:
+Special thanks to @ syedee for adding this problem and creating all test cases.
+
+Subscribe to see which companies asked this question.
+
+Hide Tags Dynamic Programming Bit Manipulation
+Hide Similar Problems (E) Number of 1 Bits
+
 class Solution {
 public:
     vector<int> countBits(int num) {
-        vector<int> res(num+1, 0);
+        vector<int> count(num+1);
+        if (num==0) return count;
         for (int i=1; i<=num; i++) {
-            res[i]=res[i>>1]+(i&0x1);
+            count[i]=count[i>>1]+(i&0x1);
         }
-        return res;
+        
+        return count;
     }
 };
+
--- cpp/increasing_triple_subsequence.cpp
+++ cpp/increasing_triple_subsequence.cpp
@@ -23,62 +23,23 @@ Hide Company Tags Facebook
 Hide Similar Problems (M) Longest Increasing Subsequence
 
 维护变量a, b，用来记录数组中大小递增的前2个元素。
-
-更确切的说：a是当前的最小元素；b是位于a之后，大于a并且距离a最近的元素。
-
-初始令a = b = None
-
 遍历数组nums，记当前元素为n
-
   若a为空或者a >= n，则令a = n；
-
   否则，若b为空或者b >= n，则令b = n；
-
   否则，返回true
-
 遍历结束时，返回false。
+[3, 6, 1, 9]
 
-  class Solution {
-  public:
-      bool increasingTriplet(vector<int>& nums) {
-    	  bool min1Found=false, min2Found=false;
-    	  int min1, min2;
-
-    	  for (int i=0; i<nums.size(); i++) {
-    		  if (min1Found==false) {
-    			  min1=nums[i];
-    			  min1Found=true;
-    		  } else if (nums[i]<=min1) {
-    			  min1=nums[i];
-    		  } else if (min2Found==false) {
-    			  min2=nums[i];
-    			  min2Found=true;
-    		  } else if (nums[i]<=min2) {
-    			  min2=nums[i];
-    		  } else {
-    			  return true;
-    		  }
-    	  }
-
-    	  return false;
-      }
-  };
-
-2.
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int min1=INT_MAX, min2=INT_MAX;
-        
-        for (size_t i=0; i<nums.size(); i++) {
-            if (nums[i]<=min1) {
-                min1=nums[i];
-            } else if (nums[i]<=min2) {
-                min2=nums[i];
-            } else {
-                return true;
-            }
+        long long a=LLONG_MAX, b=LLONG_MAX;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i]<=a) a=nums[i];
+            else if (nums[i]<=b) b=nums[i];
+            else return true;
         }
         return false;
     }
 };
+
