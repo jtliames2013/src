30ed1e9449ef9806bfc78f6b551578572a739786
jtliames2013 <jtli.ames2013@gmail.com>
2017-05-12 23:04:45 -0700

    more checkin

---
+++ cpp/concatenated-words.cpp
@@ -0,0 +1,30 @@
+472. Concatenated Words Add to List
+DescriptionHintsSubmissionsSolutions
+Total Accepted: 5251
+Total Submissions: 17783
+Difficulty: Hard
+Contributors:
+fishercoder
+Given a list of words (without duplicates), please write a program that returns all concatenated words in the given list of words.
+
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+Example:
+Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+ "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+Note:
+The number of elements of the given array will not exceed 10,000
+The length sum of elements in the given array will not exceed 600,000.
+All the input string will only include lower case letters.
+The returned elements order does not matter.
+Subscribe to see which companies asked this question.
+
+Hide Tags Dynamic Programming Trie Depth-first Search
+Hide Similar Problems (H) Word Break II
+
+
