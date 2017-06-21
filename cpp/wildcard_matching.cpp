c5e2b3074f9cacb115df631448700caa8f8204b1
jtliames2013 <jtli.ames2013@gmail.com>
2017-06-21 00:05:55 -0700

    more checkin

--- cpp/wildcard_matching.cpp
+++ cpp/wildcard_matching.cpp
@@ -24,114 +24,38 @@ Hide Similar Problems (H) Regular Expression Matching
 [解题思路]
 主要是*的匹配问题。p每遇到一个*，就保留住当前*的坐标和s的坐标，然后s从前往后扫描，如果不成功，则s++,重新扫描。
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <list>
-#include <unordered_set>
-#include <map>
-#include <algorithm>
-#include <limits.h>
-#include <math.h>
-
-using namespace std;
-
-/**
- * Definition for binary tree
- */
-struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
-/**
- * Definition for singly-linked list.
- */
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
- };
-
-/**
- * Definition for undirected graph.
- * */
-struct UndirectedGraphNode {
-    int label;
-    vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraphNode(int x) : label(x) {};
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i=0, j=0, sIndex, pIndex;
+        bool star=false;
+        
+        while (i<s.size()) {
+            if (p[j]=='?') {
+                i++;
+                j++;
+            } else if (p[j]=='*') {
+                while (p[j]=='*') j++;
+                if (j==p.size()) return true;
+                star=true;
+                sIndex=i;
+                pIndex=j;
+            } else {
+                if (s[i]==p[j]) {
+                    i++;
+                    j++;
+                } else {
+                    if (star==false) return false;
+                    else {
+                        j=pIndex;
+                        i=++sIndex;
+                    }
+                }
+            }
+        }
+        
+        while (p[j]=='*') j++;
+        return j==p.size();
+    }
 };
-
-/**
- * Definition for binary tree with next pointer.
- */
-struct TreeLinkNode {
-  int val;
-  TreeLinkNode *left, *right, *next;
-  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-};
-
-/**
- * Definition for an interval.
-*/
- struct Interval {
-      int start;
-      int end;
-      Interval() : start(0), end(0) {}
-      Interval(int s, int e) : start(s), end(e) {}
- };
-
- class Solution {
- public:
-     bool isMatch(string s, string p) {
-    	 int i=0, j=0;
-    	 bool star=false;
-    	 int sIndex, pIndex;
-    	 while (i<s.size()) {
-    		 if (p[j]!='?' && p[j]!='*') {
-    			 if (s[i]!=p[j]) {
-    				 if (star==false) {
-    					 return false;
-    				 } else {
-    					 j=pIndex;
-    					 i=++sIndex;
-    				 }
-    			 }
-    			 else {
-    				 i++;
-    				 j++;
-    			 }
-    		 } else if (p[j]=='?') {
-    			 i++;
-    			 j++;
-    		 } else {
-    			 while (p[j]=='*') j++;
-    			 if (p[j]=='\0') return true;
-    			 star=true;
-    			 sIndex=i;
-    			 pIndex=j;
-    		 }
-    	 }
-
-    	 while (p[j]=='*') j++;
-    	 if (j==p.size()) return true;
-    	 else return false;
-     }
- };
-
-int main()
-{
-	string s="ab";
-	string p="*a";
-
-	Solution sol;
-	sol.isMatch(s, p);
-	return 0;
-}
 
