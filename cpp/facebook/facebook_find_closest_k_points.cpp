246c981245fe75a7a5a5de1a51b9c9afc25ccc45
jtliames2013 <jtli.ames2013@gmail.com>
2017-09-09 18:41:35 -0700

    more checkin

--- cpp/facebook/facebook_find_closest_k_points.cpp
+++ cpp/facebook/facebook_find_closest_k_points.cpp
@@ -1,36 +1,40 @@
 struct Point {
 	int x;
 	int y;
-	int dist;
 	Point(int a, int b): x(a), y(b) {}
-	Point(int a, int b, Point orig): x(a), y(b), dist(sqrt((a-orig.x)*(a-orig.x)+(b-orig.y)*(b-orig.y))) {}
 };
 
-class Compare {
-public:
-	bool operator()(Point a, Point b) {
-		return a.dist<b.dist;
-	}
-};
-
+1. MaxHeap
 class Solution {
 public:
+	int dist(Point& a, Point& b) {
+		return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+	}
+
 	vector<Point> findClosestKPoints(vector<Point> points, Point origin, int k) {
 		vector<Point> res;
-		priority_queue<Point, vector<Point>, Compare> pq;
+		int n=points.size();
+		vector<pair<int,int>> d(n);
+		for (int i=0; i<n; i++) d[i]={dist(points[i], origin), i};
+
+		auto comp = [] (pair<int,int>& a, pair<int,int>& b) {
+			return a.first<b.first;
+		};
+
+		priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> pq(comp);
 		for (int i=0; i<points.size(); i++) {
 			if (pq.size()<k) {
-				pq.push(points[i]);
+				pq.push(d[i]);
 			} else {
-				if (points[i].dist<pq.top().dist) {
+				if (d[i].first<pq.top().first) {
 					pq.pop();
-					pq.push(points[i]);
+					pq.push(d[i]);
 				}
 			}
 		}
 
 		while (!pq.empty()) {
-			res.push_back(pq.top());
+			res.push_back(points[pq.top().second]);
 			pq.pop();
 		}
 
@@ -38,42 +42,47 @@ public:
 	}
  };
 
-2. quick select
+2. Quick Select
 class Solution {
 public:
-	int partition(vector<Point> points, int l, int r, int pivot) {
-		Point pivotValue=points[pivot];
-		swap(points[r], points[pivot]);
+	int dist(Point& a, Point& b) {
+		return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+	}
+
+	int partition(vector<pair<int,int>>& d, int l, int r, int pivot) {
+		pair<int,int> pivotValue=d[pivot];
+		swap(d[r], d[pivot]);
 		int index=l;
 		for (int i=l; i<r; i++) {
-			if (points[i].dist<pivotValue.dist) {
-				swap(points[index], points[i]);
+			if (d[i].first<pivotValue.first) {
+				if (index!=i) swap(d[index], d[i]);
 				index++;
 			}
 		}
-		swap(points[index], points[r]);
+		swap(d[index], d[r]);
 		return index;
 	}
 
-	vector<Point> findClosestKPoints(vector<Point> points, int k) {
+	vector<Point> findClosestKPoints(vector<Point> points, Point origin, int k) {
 		vector<Point> res;
 		int n=points.size();
-		if (n==0) return res;
+		if (k<=0) return res;
+		if (k>n) return points;
+
+		vector<pair<int,int>> d(n);
+		for (int i=0; i<n; i++) d[i]={dist(points[i], origin), i};
+
 		int l=0, r=n-1;
 		int pivotIndex;
 		while (1) {
-			if (l==r) {
-				pivotIndex=l;
-				break;
-			}
 			pivotIndex=rand()%(r-l+1)+l;
-			pivotIndex=partition(points, l, r, pivotIndex);
+			pivotIndex=partition(d, l, r, pivotIndex);
 			if (pivotIndex==k-1) break;
 			else if (pivotIndex>k-1) r=pivotIndex-1;
 			else l=pivotIndex+1;
 		}
 
-		for (int i=0; i<=pivotIndex; i++) res.push_back(points[i]);
+		for (int i=0; i<=pivotIndex; i++) res.push_back(points[d[i].second]);
 		return res;
 	}
  };
@@ -82,9 +91,9 @@ int main()
 {
 	Solution s;
 	Point origin(1,1);
-	vector<Point> points={Point(1, 1, origin), Point(2, 3, origin),
-			Point(-1, 5, origin), Point(3, 3, origin), Point(2, 2, origin),
-			Point(4, -1, origin), Point(5, -3, origin)};
+	vector<Point> points={Point(1, 1), Point(2, 3),
+			Point(-1, 5), Point(3, 3), Point(2, 2),
+			Point(4, -1), Point(5, -3)};
 	vector<Point> res=s.findClosestKPoints(points, origin, 2);
 	for (int i=0; i<res.size(); i++) {
 		cout << res[i].x << " " << res[i].y << endl;
--- cpp/sort_colors.cpp
+++ cpp/sort_colors.cpp
@@ -42,13 +42,11 @@ public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
         vector<int> count(3);
-        for (int i:nums) count[i]++;
-        for (int i=1; i<3; i++) count[i]+=count[i-1];
-        
-        for (int i=0; i<n; i++) {
-            if (i<count[0]) nums[i]=0;
-            else if (i<count[1]) nums[i]=1;
-            else nums[i]=2;
+        for (int i:nums) count[i]++;        
+    
+        int idx=0;
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<count[i]; j++) nums[idx++]=i;
         }
     }
 };
