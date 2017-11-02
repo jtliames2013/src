3637d0c8a5c2b0ffc225cd13791fdecc89fa20d9
jtliames2013 <jtli.ames2013@gmail.com>
2017-11-01 21:18:43 -0700

    more checkin

---
+++ cpp/1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,35 @@
+717. 1-bit and 2-bit Characters
+DescriptionHintsSubmissionsDiscussSolution
+Discuss Pick One
+We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+
+Example 1:
+Input: 
+bits = [1, 0, 0]
+Output: True
+Explanation: 
+The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+Example 2:
+Input: 
+bits = [1, 1, 1, 0]
+Output: False
+Explanation: 
+The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+Note:
+
+1 <= len(bits) <= 1000.
+bits[i] is always 0 or 1.
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n=bits.size(), i=0;
+        for (; i<n-1; ) {
+            if (bits[i]==1) i+=2;
+            else i++;
+        }
+        return i==n-1;
+    }
+};
