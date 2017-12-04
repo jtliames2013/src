cacaacbc7b0e4e18405ac047a7fcd012d72e0697
jtliames2013 <jtli.ames2013@gmail.com>
2017-12-03 16:34:29 -0800

    more checkin

---
+++ cpp/flood-fill.cpp
@@ -0,0 +1,56 @@
+733. Flood Fill
+DescriptionHintsSubmissionsDiscussSolution
+Discuss Pick One
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+Note:
+
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& res, int row, int col, int m, int n, int oldColor, int newColor) {
+        res[row][col]=newColor;
+        for (int k=0; k<4; k++) {
+            int nr=row+delta[k][0];
+            int nc=col+delta[k][1];
+            if (nr>=0 && nr<m && nc>=0 && nc<n && res[nr][nc]==oldColor) {
+                dfs(res, nr, nc, m, n, oldColor, newColor);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        vector<vector<int>> res=image;
+        int m=image.size();
+        if (m==0) return res;
+        int n=image[0].size();
+        if (n==0) return res;        
+        int oldColor=image[sr][sc];
+        if (oldColor==newColor) return res;
+        
+        dfs(res, sr, sc, m, n, oldColor, newColor);
+        
+        return res;
+    }
+private:
+    const int delta[4][2]={{-1,0}, {1,0}, {0,-1},{0,1}};
+};
+
--- cpp/uber/uber_rate_limiter.cpp
+++ cpp/uber/uber_rate_limiter.cpp
@@ -5,7 +5,7 @@ Develop an API Rate-limit Throttling Client.
 
 设计call rate limit.具体是说给你一个ip，你先chec是否有大于100个call from this ip since last second. 大于100就throw exception，没有的话就 do sth。
 
-
+1. token bucket
 #include <chrono>
 #include <ctime>
 #include <thread>
@@ -29,14 +29,13 @@ public:
 		auto currtime=system_clock::now();
 		duration<double> diff = currtime-lasttime;
 		tokens=min(capacity, tokens+long(diff.count()*rate));
-		lasttime=currtime;
 
-		if (tokens<=0) {
-			return false;
-		} else {
+		if (tokens>0) {
 			tokens--;
+			lasttime=currtime;
 			return true;
 		}
+		return false;
 	}
 
 	duration<double> take(long count) {
@@ -107,7 +106,12 @@ public:
 int main() {
 	Client client;
 
-	for (int i=0; i<150; i++) {
+	for (int i=0; i<100; i++) {
+		bool b=client.tryAccept("10.204.0.1");
+		cout << "i=" << i << " " << b << endl;
+	}
+	for (int i=100; i<150; i++) {
+		this_thread::sleep_for(duration<double>(0.005));
 		bool b=client.tryAccept("10.204.0.1");
 		cout << "i=" << i << " " << b << endl;
 	}
@@ -121,5 +125,126 @@ int main() {
 	}
 
 	return 0;
+}
+
+2. Hit count
+class HitCount {
+public:
+	HitCount() {
+		hits=0;
+	}
+
+	bool tryAccept(time_point<system_clock> timestamp) {
+		removeHits(timestamp);
+		if (hits>=capacity) return false;
+		if (!q.empty() && q.back().first==timestamp) q.back().second++;
+		else q.push({timestamp, 1});
+		hits++;
+		return true;
+	}
+
+private:
+	void removeHits(time_point<system_clock> timestamp) {
+		while (!q.empty() && duration<double>(timestamp-q.front().first).count()>=span) {
+			hits-=q.front().second;
+			q.pop();
+		}
+	}
+
+	queue<pair<time_point<system_clock>,int>> q;
+	int hits;
+	const int capacity=100;
+	const int span=1;
+};
+
+class Client {
+public:
+	Client() {}
+
+	bool tryAccept(string ip) {
+		lock_guard<mutex> guard(mx);
+		if (mp.find(ip)==mp.end()) {
+			mp[ip]=new HitCount();
+		}
+
+		return mp[ip]->tryAccept(system_clock::now());
+	}
+
+	unordered_map<string, HitCount*> mp;
+	mutex mx;
+};
+
+int main() {
+	Client client;
+
+	for (int i=0; i<100; i++) {
+		bool b=client.tryAccept("10.204.0.1");
+		cout << "i=" << i << " " << b << endl;
+	}
+
+	for (int i=100; i<150; i++) {
+		bool b=client.tryAccept("10.204.0.1");
+		this_thread::sleep_for(milliseconds(100));
+		cout << "i=" << i << " " << b << endl;
+	}
+
+	return 0;
+}
+
+3. Leaky bucket
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Bucket struct {
+	mu       sync.Mutex
+	capacity int64
+	rate     int64
+	water    int64
+	lasttime time.Time
+}
+
+func NewBucket(rate, capacity int64) *Bucket {
+	b := &Bucket{
+		capacity: capacity,
+		rate:     rate,
+		lasttime: time.Now(),
+	}
+
+	return b
+}
+
+func (b *Bucket) tryAccept() bool {
+	currtime := time.Now()
+	left := b.water - int64(float64(b.rate)*currtime.Sub(b.lasttime).Seconds())
+	if left > 0 {
+		b.water = left
+	} else {
+		b.water = 0
+	}
+
+	if b.water < b.capacity {
+		b.water++
+		b.lasttime = currtime
+		return true
+	}
+	return false
+}
+
+func main() {
+	b := NewBucket(100, 100)
+	for i := 0; i < 100; i++ {
+		res := b.tryAccept()
+		fmt.Println(i, res)
+	}
+	for i := 100; i < 150; i++ {
+		time.Sleep(5 * time.Millisecond)
+		res := b.tryAccept()
+		fmt.Println(i, res)
+	}
 }
 
