fa7dc9e2c2e911d21be5df45eb17d599a308a495
jtliames2013 <jtli.ames2013@gmail.com>
2017-12-09 16:40:27 -0800

    more checkin

--- cpp/square/square_search_range.cpp
+++ cpp/square/square_search_range.cpp
@@ -1,50 +1,87 @@
-2. 给一组数 [10,4,6,7,8,9], 然后给一个range 比如是 [1,5], 求出这个数组中所有的在这个range中的数.
-    这题和 leetcode里 search range 很类似. 主要是 binary search.
-    follow up 是如果数组中存在重复的数怎么处理, 反正search range的解法可以handle这个.
-    这一轮面试面试官迟到了10分钟,然后聊的也比较开心和谐. 很快就写完了,算上面试官迟到的10分钟一共就30分钟, 面试官就说他OK了, 然后问他问题. 
+给一组数 [4,6,7,8,9,10], 然后给一个range 比如是 [1,5], 求出这个数组中所有的在这个range中的数.
+这题和 leetcode里 search range 很类似. 主要是 binary search.
+follow up 是如果数组中存在重复的数怎么处理, 反正search range的解法可以handle这个.
+这一轮面试面试官迟到了10分钟,然后聊的也比较开心和谐. 很快就写完了,算上面试官迟到的10分钟一共就30分钟, 面试官就说他OK了, 然后问他问题. 
 
+电面，类似于range search，实现两个接口，add 添加value，search(lower, upper)，找到所有在这个区间内的元素，并且按照顺序返回一个list
+
+1. Use array
 class Solution {
 public:
-	vector<int> search(vector<int>& nums, int start, int end) {
-		vector<int> res;
-		for (int i=start; i<=end; i++) {
-			if (search(nums, i)) res.push_back(i);
-		}
-		return res;
-	}
-
-	bool search(vector<int>& nums, int target) {
-		int l=0, r=nums.size()-1;
-		int mid;
-
-		while (l<=r) {
+	void add(int val) {
+		size_t l=0, r=nums.size(), mid;
+		while (l<r) {
 			mid=l+(r-l)/2;
-			if (nums[mid]==target) return true;
-			if (nums[mid]<nums[r]) {
-				if (target>nums[mid] && target<=nums[r]) {
-					l=mid+1;
-				} else {
-					r=mid-1;
-				}
-			} else if (nums[mid]>nums[r]){
-				if (target>=nums[l] && target<nums[mid]) {
-					r=mid-1;
-				} else {
-					l=mid+1;
-				}
-			} else {
-				r--;
-			}
+			if (nums[mid]<val) l=mid+1;
+			else r=mid;
 		}
-
-		return false;
+		nums.insert(nums.begin()+r, val);
 	}
+
+	vector<int> search(int lower, int upper) {
+		size_t l=0, r=nums.size(), mid;
+		while (l<r) {
+			mid=l+(r-l)/2;
+			if (nums[mid]<lower) l=mid+1;
+			else r=mid;
+		}
+		int start=r;
+		l=r; r=nums.size();
+		while (l<r) {
+			mid=l+(r-l)/2;
+			if (nums[mid]<=upper) l=mid+1;
+			else r=mid;
+		}
+		int end=r;
+
+		return vector<int>(nums.begin()+start, nums.begin()+end);
+	}
+private:
+	vector<int> nums;
 };
+
+2. Use multiset
+class Solution {
+public:
+	void add(int val) {
+		data.insert(val);
+	}
+
+	vector<int> search(int lower, int upper) {
+		auto left=lower_bound(data.begin(), data.end(), lower);
+		if (left==data.end()) return vector<int> ();
+		auto right=upper_bound(data.begin(), data.end(), upper);
+
+		return vector<int>(left, right);
+	}
+private:
+	multiset<int> data;
+};
+
 
 int main() {
 	Solution s;
-	vector<int> nums={10,4,6,7,8,9};
-	vector<int> res=s.search(nums, 1, 5);
+	for (int i=0; i<10; i++) {
+		s.add(i);
+		s.add(i);
+	}
+
+	vector<int> res=s.search(3, 8);
+	for (auto i:res) cout << i << " ";
+	cout << endl;
+
+	vector<int> res2=s.search(20, 30);
+	for (auto i:res2) cout << i << " ";
+	cout << endl;
+
+	vector<int> res3=s.search(3, 20);
+	for (auto i:res3) cout << i << " ";
+	cout << endl;
+
+	vector<int> res4=s.search(-10, -1);
+	for (auto i:res4) cout << i << " ";
+	cout << endl;
+
 	return 0;
 }
 
---
+++ python/utf-8-validation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+        i=0
+        count=0
+        while i<len(data):
+            if (data[i]>>7) == 0: count=0
+            elif (data[i]>>5) == 0b110: count=1
+            elif (data[i]>>4) == 0b1110: count=2
+            elif (data[i]>>3) == 0b11110: count=3
+            else: return False
+            
+            i+=1
+            if i+count>len(data): return False
+            j=i
+            while j<i+count:
+                if (data[j]>>6) != 0b10: return False
+                j+=1
+            i=j
+            
+        return True
