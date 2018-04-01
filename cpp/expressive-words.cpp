c9f8c933c27e404052fb9bf295c8ff1bf60ea22f
jtliames2013 <jtli.ames2013@gmail.com>
2018-04-01 14:23:19 -0700

    more checkin

---
+++ cpp/expressive-words.cpp
@@ -0,0 +1,48 @@
+809. Expressive Words
+DescriptionHintsSubmissionsDiscussSolution
+Sometimes people repeat letters to represent extra feeling, such as "hello" -> "heeellooo", "hi" -> "hiiii".  Here, we have groups, of adjacent letters that are all the same character, and adjacent characters to the group are different.  A group is extended if that group is length 3 or more, so "e" and "o" would be extended in the first example, and "i" would be extended in the second example.  As another example, the groups of "abbcccaaaa" would be "a", "bb", "ccc", and "aaaa"; and "ccc" and "aaaa" are the extended groups of that string.
+
+For some given string S, a query word is stretchy if it can be made to be equal to S by extending some groups.  Formally, we are allowed to repeatedly choose a group (as defined above) of characters c, and add some number of the same character c to it so that the length of the group is 3 or more.  Note that we cannot extend a group of size one like "h" to a group of size two like "hh" - all extensions must leave the group extended - ie., at least 3 characters long.
+
+Given a list of query words, return the number of words that are stretchy. 
+
+Example:
+Input: 
+S = "heeellooo"
+words = ["hello", "hi", "helo"]
+Output: 1
+Explanation: 
+We can extend "e" and "o" in the word "hello" to get "heeellooo".
+We can't extend "helo" to get "heeellooo" because the group "ll" is not extended.
+Notes:
+
+0 <= len(S) <= 100.
+0 <= len(words) <= 100.
+0 <= len(words[i]) <= 100.
+S and all words in words consist only of lowercase letters
+
+class Solution {
+public:
+    bool isStretchy(string& S, string& w) {
+        if (S.size()<w.size()) return false;
+        int i1=0, i2=0;
+        for (; i1<S.size() && i2<w.size(); ) {
+            int c1=0, c2=0;
+            if (S[i1]!=w[i2]) return false;
+            char ch=S[i1];
+            while (i1<S.size() && S[i1]==ch) { i1++; c1++; }
+            while (i2<w.size() && w[i2]==ch) { i2++; c2++; }
+            if (c1<c2 || (c1<3 && c1!=c2)) return false;
+        }
+        return i1==S.size() && i2==w.size();
+    }
+    
+    int expressiveWords(string S, vector<string>& words) {
+        int res=0;
+        for (auto w:words) {
+            if (isStretchy(S, w)) res++;
+        }
+        
+        return res;
+    }
+};
