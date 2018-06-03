6b6a9e08c9f74df43c5c151fa5984ba773f21f87
jtliames2013 <jtli.ames2013@gmail.com>
2018-06-02 20:16:01 -0700

    more checkin

---
+++ python/flipping-an-image.py
@@ -0,0 +1,7 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[ i^1 for i in r[::-1]] for r in A]
