68514459c0b1152dfdaf1f76d51bb8b3c2b060fb
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 14:21:17 -0700

    more checkin

--- cpp/excel-sheet-column-number.cpp
+++ cpp/excel-sheet-column-number.cpp
@@ -16,11 +16,11 @@ For example:
 class Solution {
 public:
     int titleToNumber(string s) {
-        int res=0;
+        unsigned int res=0;
         for (auto c:s) {
-            res=res*26+c-'A'+1;
+            res=res*26+c-'A'+1;            
         }
+        
         return res;
     }
 };
-
