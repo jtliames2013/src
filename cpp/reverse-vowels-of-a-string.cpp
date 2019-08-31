4206805287a7bb8dcf3b58a1b2fe6e2fb14026c3
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 16:09:03 -0700

    more checkin

--- cpp/reverse-vowels-of-a-string.cpp
+++ cpp/reverse-vowels-of-a-string.cpp
@@ -22,22 +22,22 @@ Hide Similar Problems (E) Reverse String
 
 class Solution {
 public:
+    string reverseVowels(string s) {        
+        int l=0, r=s.size()-1;
+        while (l<r) {
+            if (!isVowel(s[l])) l++;
+            else if (!isVowel(s[r])) r--;
+            else {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+        }
+        return s;
+    }
+private:
     bool isVowel(char c) {
         return (c=='a' || c=='i' || c=='e' || c=='o' || c=='u' ||
                 c=='A' || c=='I' || c=='E' || c=='O' || c=='U');
     }
-
-    string reverseVowels(string s) {
-        int n=s.size();
-        int l=0, r=n-1;
-        while (l<r) {
-            while (l<r && !isVowel(s[l])) l++;
-            while (l<r && !isVowel(s[r])) r--;
-            swap(s[l], s[r]);
-            l++;
-            r--;
-        }
-        return s;
-    }
 };
-
