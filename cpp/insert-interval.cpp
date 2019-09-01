ee6e57c3a2e42be9b1bbef8a774ceb0b71c7799f
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 21:15:00 -0700

    more checkin

--- cpp/insert-interval.cpp
+++ cpp/insert-interval.cpp
@@ -52,7 +52,6 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
         int n=intervals.size(), i=0;       
-
         for (; i<n; ++i) {
             if (intervals[i][1]<newInterval[0]) res.push_back(intervals[i]);
             else if (intervals[i][0]>newInterval[1]) break;
@@ -61,7 +60,6 @@ public:
                 newInterval[1]=max(newInterval[1], intervals[i][1]);
             }
         }
-        
         res.push_back(newInterval);
         for (; i<n; ++i) res.push_back(intervals[i]);
                 
