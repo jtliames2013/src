3e89f1d9786929f5722637cc32887cfba9ecf405
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 20:45:10 -0700

    more checkin

--- cpp/merge-intervals.cpp
+++ cpp/merge-intervals.cpp
@@ -57,3 +57,21 @@ public:
         return intervals;
     }
 };
+
+3. vector as Interval
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        int n=intervals.size();
+        if (n==0) return res;
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) { return a[0]<b[0]; });
+        res.push_back(intervals[0]);
+        for (int i=1; i<n; ++i) {
+            if (res.back()[1]<intervals[i][0]) res.push_back(intervals[i]);
+            else res.back()[1]=max(res.back()[1], intervals[i][1]);
+        }
+        
+        return res;
+    }
+};
