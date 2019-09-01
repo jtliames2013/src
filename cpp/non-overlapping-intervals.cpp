344417c8924b0afcef676523d59eb2c47ef6113d
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 21:12:10 -0700

    more checkin

--- cpp/non-overlapping-intervals.cpp
+++ cpp/non-overlapping-intervals.cpp
@@ -57,3 +57,19 @@ public:
     }
 };
 
+2. vector as Interval
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        if (n==0) return 0;
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b){ return a[1]<b[1]; });
+        
+        int end=intervals[0][1], count=0;
+        for (int i=1; i<n; ++i) {
+            if (intervals[i][0]<end) count++;
+            else end=intervals[i][1];
+        }
+        return count;
+    }
+};
