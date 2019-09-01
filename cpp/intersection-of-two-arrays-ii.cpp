f60236bc276544400c5390f714ab8fc1aa2ad2d2
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-01 11:05:03 -0700

    more checkin

--- cpp/intersection-of-two-arrays-ii.cpp
+++ cpp/intersection-of-two-arrays-ii.cpp
@@ -30,21 +30,21 @@ public:
         if (n1==0 || n2==0) return res;
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-        for (int i=0, j=0; i<n1 && j<n2; ) {
-            if (nums1[i]<nums2[j]) i++;
-            else if (nums1[i]>nums2[j]) j++;
+        
+        for (int i1=0, i2=0; i1<n1 && i2<n2; ) {
+            if (nums1[i1]<nums2[i2]) i1++;
+            else if (nums1[i1]>nums2[i2]) i2++;
             else {
-                res.push_back(nums1[i]);
-                i++;
-                j++;
+                res.push_back(nums1[i1]);
+                i1++;
+                i2++;
             }
         }
-        
         return res;
     }
 };
 
-2. Use set
+2. Use map
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
