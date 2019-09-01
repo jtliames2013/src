c4db100e6ac90dbf7a8a97d42c40377afe546c04
jtliames2013 <jtli.ames2013@gmail.com>
2019-08-31 21:14:22 -0700

    more checkin

--- cpp/insert-interval.cpp
+++ cpp/insert-interval.cpp
@@ -51,8 +51,7 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
-        int n=intervals.size(), i=0;        
-        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b){ return a[0]<b[0]; });
+        int n=intervals.size(), i=0;       
 
         for (; i<n; ++i) {
             if (intervals[i][1]<newInterval[0]) res.push_back(intervals[i]);
