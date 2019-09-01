4057222d22b300e02b0d7fec772e9f7ef14b084c
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-01 14:29:59 -0700

    more checkin

--- cpp/merge-sorted-array.cpp
+++ cpp/merge-sorted-array.cpp
@@ -17,13 +17,11 @@ Hide Similar Problems (E) Merge Two Sorted Lists
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index=m+n-1, i=m-1, j=n-1;
-        while (i>=0&&j>=0) {
-            if (nums1[i]>nums2[j]) nums1[index--]=nums1[i--];
-            else nums1[index--]=nums2[j--];
+        int i=m-1, j=n-1, k=m+n-1;
+        while (i>=0 && j>=0) {
+            if (nums1[i]>nums2[j]) nums1[k--]=nums1[i--];
+            else nums1[k--]=nums2[j--];
         }
-        while (j>=0) {
-            nums1[index--]=nums2[j--];
-        }
+        while (j>=0) nums1[k--]=nums2[j--];
     }
 };
