87d22ef5f25237f952ece6261296cfd472e8c677
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-01 18:19:02 -0700

    more checkin

--- cpp/minimum-depth-of-binary-tree.cpp
+++ cpp/minimum-depth-of-binary-tree.cpp
@@ -19,15 +19,9 @@ class Solution {
 public:
     int minDepth(TreeNode* root) {
         if (root==NULL) return 0;
-        if (root->left==NULL&&root->right==NULL) return 1;
         int depth=INT_MAX;
-        if (root->left) {
-            depth=min(depth,minDepth(root->left)+1);
-        }
-        if (root->right) {
-            depth=min(depth,minDepth(root->right)+1);
-        }
-        return depth;
+        if (root->left) depth=min(depth, minDepth(root->left));
+        if (root->right) depth=min(depth, minDepth(root->right));
+        return depth==INT_MAX?1:depth+1;
     }
 };
-
