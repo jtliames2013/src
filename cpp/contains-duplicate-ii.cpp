b379565fca6118ecaa3702416ab7d24c0ceecc87
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-02 11:28:43 -0700

    more checkin

--- cpp/contains-duplicate-ii.cpp
+++ cpp/contains-duplicate-ii.cpp
@@ -5,6 +5,20 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> st;
+        for (int i=0; i<nums.size(); ++i) {
+            if (i>k) st.erase(nums[i-k-1]);
+            if (st.find(nums[i])!=st.end()) return true;
+            st.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
+2.
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> st;
         for (int i=0; i<nums.size(); i++) {
             if (st.find(nums[i])!=st.end()) return true;
             st.insert(nums[i]);
