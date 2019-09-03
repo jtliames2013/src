03dd336ee212238b4d19e669dedd7e7fcd356dcd
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-03 12:12:39 -0700

    more checkin

--- cpp/fizz-buzz.cpp
+++ cpp/fizz-buzz.cpp
@@ -31,12 +31,11 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> res(n);
-        for (int i=1; i<=n; i++) {
+        for (int i=1; i<=n; ++i) {
             string str;
             if (i%3==0) str+="Fizz";
             if (i%5==0) str+="Buzz";
-            if (str.empty()) str+=to_string(i);
-            res[i-1]=str;
+            res[i-1]=str.empty()?to_string(i):str;
         }
         return res;
     }
