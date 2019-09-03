5cd611abcad762463f7b098a6a0f807381ec6905
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-03 11:38:44 -0700

    more checkin

--- cpp/is-subsequence.cpp
+++ cpp/is-subsequence.cpp
@@ -55,3 +55,14 @@ public:
     }
 };
 
+3.
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        for (auto c:t) {
+            if (c==s[i]) i++;
+        }
+        return i==s.size();
+    }
+};
