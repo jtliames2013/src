99f1b2a06b05091388826918d7ac2dbe8c704c43
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-03 22:36:31 -0700

    more checkin

--- cpp/add-binary.cpp
+++ cpp/add-binary.cpp
@@ -14,20 +14,16 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string res;
-        int m=a.size(), n=b.size();
-        int i=m-1, j=n-1;
-        int num=0;
-        while (i>=0 || j>=0) {            
+        int i=a.size()-1, j=b.size()-1, num=0;
+        while (i>=0 || j>=0) {
             if (i>=0) num+=a[i--]-'0';
             if (j>=0) num+=b[j--]-'0';
             res.push_back(num%2+'0');
             num/=2;
         }
         
-        if (num!=0) res.push_back(num+'0');
-        
+        if (num>0) res.push_back(num+'0');
         reverse(res.begin(), res.end());
         return res;
     }
 };
-
