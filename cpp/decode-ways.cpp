f762eb8c467dab3402ef7917b5aa18f54ee19421
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-04 17:59:57 -0700

    more checkin

--- cpp/decode-ways.cpp
+++ cpp/decode-ways.cpp
@@ -20,12 +20,12 @@ public:
     int numDecodings(string s) {
         int n=s.size();
         if (n==0) return 0;
-        int a=1, b=s[0]=='0'?0:1, c;
-        for (int i=1; i<n; i++) {
+        unsigned int a=1, b=s[0]=='0'?0:1, c;
+        for (int i=1; i<n; ++i) {
             c=0;
-            if (s[i]=='0' && s[i-1]=='0') return 0;
-            if (s[i-1]=='1' || (s[i-1]=='2' && s[i]<='6')) c+=a;
+            if (s[i]=='0' && (s[i-1]<'1' || s[i-1]>'2')) return 0;
             if (s[i]!='0') c+=b;
+            if (s[i-1]=='1' || (s[i-1]=='2' && s[i]<='6')) c+=a;
             a=b;
             b=c;
         }
