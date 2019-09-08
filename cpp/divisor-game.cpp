b0cee4257825f0333a149bd399d283ab8a363475
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-08 11:10:40 -0700

    more checkin

--- cpp/divisor-game.cpp
+++ cpp/divisor-game.cpp
@@ -36,6 +36,20 @@ Note:
 
 1 <= N <= 1000
 
+Recursive Prove （Top-down)
+If N is even.
+We can choose x = 1.
+The opponent will get N - 1, which is a odd.
+Reduce to the case odd and he will lose.
+
+If N is odd,
+2.1 If N = 1, lose directly.
+2.2 We have to choose an odd x.
+The opponent will get N - x, which is a even.
+Reduce to the case even and he will win.
+
+So the N will change odd and even alternatively until N = 1.
+
 class Solution {
 public:
     bool divisorGame(int N) {
