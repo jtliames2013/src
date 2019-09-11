ae25bdf1afbc195ab8c5bfee8514612f53fadc56
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-11 14:46:31 -0700

    more checkin

--- cpp/sum-of-two-integers.cpp
+++ cpp/sum-of-two-integers.cpp
@@ -17,7 +17,7 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while (b!=0) {
-            int carry=a&b;
+            unsigned int carry=a&b;
             a^=b;
             b=carry<<1;
         }
