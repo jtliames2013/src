eb28ddbf49cd9fda802589b7cd518e3e174361ea
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-15 14:27:17 -0700

    more checkin

--- cpp/non-decreasing-array.cpp
+++ cpp/non-decreasing-array.cpp
@@ -23,15 +23,16 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int cnt=0;
-        for (int i=1; i<nums.size() && cnt<=1; i++) {
-            if (nums[i-1]>nums[i]) {
+        for (int i=1; i<nums.size(); ++i) {
+            if (nums[i]<nums[i-1]) {
                 cnt++;
+                if (cnt>1) return false;
+
                 // either lower nums[i-1] or raise nums[i]
-                if (i<2 || nums[i-2]<=nums[i]) nums[i-1]=nums[i];
+                if (i<2 || nums[i]>nums[i-2]) nums[i-1]=nums[i];
                 else nums[i]=nums[i-1];
             }
         }
-        return cnt<=1;
+        return true;
     }
 };
-
