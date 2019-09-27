56557c6b16771c8d74107cfb5ef694f392b9d13c
jtliames2013 <jtli.ames2013@gmail.com>
2019-09-26 23:09:41 -0700

    more checkin

--- cpp/teemo-attacking.cpp
+++ cpp/teemo-attacking.cpp
@@ -67,3 +67,18 @@ public:
     }
 };
 
+3.
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n=timeSeries.size();
+        if (n==0) return 0;
+        int res=0;
+        for (int i=0; i<n-1; ++i) {
+            if (timeSeries[i+1]-timeSeries[i]>duration) res+=duration;
+            else res+=(timeSeries[i+1]-timeSeries[i]);
+        }
+        res+=duration;
+        return res;
+    }
+};
