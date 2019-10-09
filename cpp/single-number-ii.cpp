b2b1bc2dee1e628024164947689642ca36bde83b
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-08 23:27:25 -0700

    more checkin

--- cpp/single-number-ii.cpp
+++ cpp/single-number-ii.cpp
@@ -13,19 +13,17 @@ Hide Similar Problems (E) Single Number (M) Single Number III
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> count(32, 0);
+        int res=0;
+        vector<int> count(32);
         
-        for (int i=0; i<nums.size(); i++) {
-            for (int j=0; j<32; j++) {
-                count[j] += (nums[i] & (0x1<<j))?1:0;
+        for (auto& n:nums) {
+            for (int i=0; i<32; ++i) {
+                count[i]+=((n>>i)&0x1);
             }
         }
-        
-        int res=0;
-        for (int i=0; i<32; i++) {
-            if (count[i]%3!=0) res |= (0x1<<i);
+        for (int i=0; i<32; ++i) {
+            if (count[i]%3!=0) res|=(1<<i);
         }
         return res;
     }
 };
-
