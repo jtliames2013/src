69d67758c544a9fcb50a8e0d7ef3b9e4542f8a2a
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-12 16:27:56 -0700

    more checkin

--- cpp/flatten-a-multilevel-doubly-linked-list.cpp
+++ cpp/flatten-a-multilevel-doubly-linked-list.cpp
@@ -1,21 +1,85 @@
-Given a linked list where in addition to the next pointer, each node has a child pointer, which may or may not point to a separate list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in below figure.You are given the head of the first level of the list. Flatten the list so that all the nodes appear in a single-level linked list. You need to flatten the list in way that all nodes at first level should come first, then nodes of second level, and so on.
+430. Flatten a Multilevel Doubly Linked List
+Medium
 
-The problem clearly say that we need to flatten level by level. The idea of solution is, we start from first level, process all nodes one by one, if a node has a child, then we append the child at the end of list, otherwise we don’t do anything. After the first level is processed, all next level nodes will be appended after first level. Same process is followed for the appended nodes.
+507
 
-void flattenList(Node* head) {
-	if (head==NULL) return;
-	Node* tail=head;
-	while (tail->next!=NULL) tail=tail->next;
+86
 
-	Node *curr=head;
-	while (curr!=tail) {
-		if (curr->child) {
-			tail->next=curr->child;
-			curr->child=NULL;
-			while (tail->next!=NULL) tail=tail->next;
-		}
+Favorite
 
-		curr=curr->next;
-	}
-}
+Share
+You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
 
+Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
+
+ 
+
+Example:
+
+Input:
+ 1---2---3---4---5---6--NULL
+         |
+         7---8---9---10--NULL
+             |
+             11--12--NULL
+
+Output:
+1-2-3-7-8-11-12-9-10-4-5-6-NULL
+ 
+
+Explanation for the above example:
+
+Given the following multilevel doubly linked list:
+
+
+ 
+
+We should return the following flattened doubly linked list:
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+
+    Node() {}
+
+    Node(int _val, Node* _prev, Node* _next, Node* _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+*/
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        dfs(head);
+        return head;
+    }
+private:
+    Node* dfs(Node* head) {
+        if (head==NULL) return NULL;
+        Node *curr=head, *prev=NULL, *next;
+        while (curr) {
+            next=curr->next;
+            if (curr->child) {
+                Node *tail=dfs(curr->child);
+                curr->next=curr->child;
+                curr->child->prev=curr;
+                tail->next=next;
+                if (next) next->prev=tail;
+                curr->child=NULL;
+                prev=tail;
+            } else {
+                prev=curr;
+            }
+            curr=next;
+        }
+        return prev;
+    }
+};
