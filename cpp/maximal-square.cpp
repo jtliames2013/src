b03d8aab4a7dc78eab2ec6467f6239376ca153fb
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-12 16:11:56 -0700

    more checkin

--- cpp/maximal-square.cpp
+++ cpp/maximal-square.cpp
@@ -22,12 +22,12 @@ public:
         if (m==0) return 0;
         int n=matrix[0].size();
         if (n==0) return 0;
-        int maxLen=INT_MIN;
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        for (int i=1; i<=m; i++) {
-            for (int j=1; j<=n; j++) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        int maxLen=0;
+        for (int i=1; i<=m; ++i) {
+            for (int j=1; j<=n; ++j) {
                 if (matrix[i-1][j-1]=='1') {
-                    dp[i][j]=min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+                    dp[i][j]=min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+1;
                     maxLen=max(maxLen, dp[i][j]);
                 }
             }
@@ -35,4 +35,3 @@ public:
         return maxLen*maxLen;
     }
 };
-
