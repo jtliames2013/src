4a7823db476f1e891a30be223e19223508711fa7
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-12 11:48:37 -0700

    more checkin

---
+++ cpp/statistics-from-a-large-sample.cpp
@@ -0,0 +1,67 @@
+1093. Statistics from a Large Sample
+Medium
+
+31
+
+162
+
+Favorite
+
+Share
+We sampled integers between 0 and 255, and stored the results in an array count:  count[k] is the number of integers we sampled equal to k.
+
+Return the minimum, maximum, mean, median, and mode of the sample respectively, as an array of floating point numbers.  The mode is guaranteed to be unique.
+
+(Recall that the median of a sample is:
+
+The middle element, if the elements of the sample were sorted and the number of elements is odd;
+The average of the middle two elements, if the elements of the sample were sorted and the number of elements is even.)
+ 
+
+Example 1:
+
+Input: count = [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,3.00000,2.37500,2.50000,3.00000]
+Example 2:
+
+Input: count = [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,4.00000,2.18182,2.00000,1.00000]
+ 
+
+Constraints:
+
+count.length == 256
+1 <= sum(count) <= 10^9
+The mode of the sample that count represents is unique.
+Answers within 10^-5 of the true value will be accepted as correct.
+
+class Solution {
+public:
+    vector<double> sampleStats(vector<int>& count) {
+        vector<double> res={255, 0, 0, 0, 0};
+        double sum=0;
+        int total=accumulate(count.begin(), count.end(), 0), maxCount=0;
+        int m1=(total-1)/2, m2=total/2;
+        for (int i=0, cnt=0; i<count.size(); cnt+=count[i], ++i) {
+            if (count[i]>0) {
+                // min
+                if (res[0]>i) res[0]=i;
+                // max
+                res[1]=i;
+                // mean
+                sum+=i*count[i];                
+                // median
+                if (m1>=cnt && m1<cnt+count[i]) res[3]+=i;
+                if (m2>=cnt && m2<cnt+count[i]) res[3]+=i;
+                // mode
+                if (count[i]>maxCount) {
+                    maxCount=count[i];
+                    res[4]=i;
+                }
+            }
+        }
+        res[2]=sum/total;
+        res[3]/=2;
+        return res;
+    }
+};
