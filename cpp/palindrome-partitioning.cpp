2d6a3c30ee42529e67197d209a9e7f6603c4652b
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-13 14:33:45 -0700

    more checkin

--- cpp/palindrome-partitioning.cpp
+++ cpp/palindrome-partitioning.cpp
@@ -15,80 +15,77 @@ Hide Company Tags Bloomberg
 Hide Tags Backtracking
 Hide Similar Problems (H) Palindrome Partitioning II
 
+1.
 class Solution {
 public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> output;
+        dfs(res, s, output, 0);
+        return res;
+    }
+private:
+    void dfs(vector<vector<string>>& res, string&s, vector<string>& output, int start) {
+        if (start==s.size()) {
+            res.push_back(output);
+            return;
+        }
+        for (int i=start; i<s.size(); ++i) {
+            if (isPalin(s, start, i)) {
+                output.push_back(s.substr(start, i-start+1));
+                dfs(res, s, output, i+1);
+                output.pop_back();
+            }
+        }
+    }
+
     bool isPalin(string& s, int start, int end) {
         while (start<end) {
             if (s[start]!=s[end]) return false;
             start++;
             end--;
         }
-        return true;    
-    }
-    
-    void partition(vector<vector<string>>& res, string& s, vector<string>& output, int start) {
-        if (start==s.size()) {
-            res.push_back(output);
-            return;
-        }
-        
-        for (int i=start; i<s.size(); i++) {
-            if (isPalin(s, start, i)) {
-                output.push_back(s.substr(start, i-start+1));
-                partition(res, s, output, i+1);
-                output.pop_back();
-            }
-        }
-    }
-    
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> output;
-        partition(res, s, output,  0);
-        
-        return res;
+        return true;
     }
 };
 
 2. Cache isPalindrome result
 class Solution {
 public:
-    void buildIsPalin(string& s, vector<vector<bool>>& isPalin) {
-        for (int i=s.size()-1; i>=0; i--) {
-            for (int j=i; j<s.size(); j++) {
-                if (j==i) isPalin[i][j]=true;
-                else if (j==i+1) isPalin[i][j]=s[i]==s[j];
-                else isPalin[i][j]=(s[i]==s[j] && isPalin[i+1][j-1]);
-            }
-        }
-    }
-    
-    void partition(vector<vector<string>>& res, string& s, vector<string>& output, vector<vector<bool>>& isPalin, int start) {
-        if (start==s.size()) {
-            res.push_back(output);
-            return;
-        }
-        
-        for (int i=start; i<s.size(); i++) {
-            if (isPalin[start][i]) {
-                output.push_back(s.substr(start, i-start+1));
-                partition(res, s, output, isPalin, i+1);
-                output.pop_back();
-            }
-        }
-    }
-    
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         int n=s.size();
         if (n==0) return res;
         vector<string> output;
-        vector<vector<bool>> isPalin(n, vector<bool>(n, false));
+        vector<vector<bool>> isPalin(n, vector<bool>(n));
         buildIsPalin(s, isPalin);
-        
-        partition(res, s, output, isPalin, 0);
-        
+        dfs(res, s, output, isPalin, 0);
         return res;
+    }
+private:
+    void buildIsPalin(string& s, vector<vector<bool>>& isPalin) {
+        int n=s.size();
+        for (int i=n-1; i>=0; --i) {
+            for (int j=i; j<n; ++j) {
+                if (i==j) isPalin[i][j]=true;
+                else if (i+1==j) isPalin[i][j]=s[i]==s[j];
+                else isPalin[i][j]=(s[i]==s[j] && isPalin[i+1][j-1]);
+            }
+        }
+    }
+    
+    void dfs(vector<vector<string>>& res, string&s, vector<string>& output, vector<vector<bool>>& isPalin, int start) {
+        if (start==s.size()) {
+            res.push_back(output);
+            return;
+        }
+        for (int i=start; i<s.size(); ++i) {
+            if (isPalin[start][i]) {
+                output.push_back(s.substr(start, i-start+1));
+                dfs(res, s, output, isPalin, i+1);
+                output.pop_back();
+            }
+        }
     }
 };
 
