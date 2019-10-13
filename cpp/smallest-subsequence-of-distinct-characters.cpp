9bec847accbe7d420218fe430866854f47b487e1
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-12 19:03:07 -0700

    more checkin

--- cpp/smallest-subsequence-of-distinct-characters.cpp
+++ cpp/smallest-subsequence-of-distinct-characters.cpp
@@ -35,4 +35,24 @@ Note:
 1 <= text.length <= 1000
 text consists of lowercase English letters.
 
-
+class Solution {
+public:
+    string smallestSubsequence(string text) {
+        string res;
+        vector<int> count(26);
+        vector<char> visited(26);
+        for (auto c:text) count[c-'a']++;
+        for (auto c:text) {
+            count[c-'a']--;
+            if (visited[c-'a']==false) {
+                while (!res.empty() && c<res.back() && count[res.back()-'a']>0) {
+                    visited[res.back()-'a']=false;
+                    res.pop_back();
+                }
+                visited[c-'a']=true;
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
