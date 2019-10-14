097a4ec8d43d1a40a994d53cbe7ad9cf75ddbd3f
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-13 21:57:01 -0700

    more checkin

--- cpp/search-a-2d-matrix.cpp
+++ cpp/search-a-2d-matrix.cpp
@@ -17,6 +17,28 @@ Given target = 3, return true.
 Hide Tags Array Binary Search
 Hide Similar Problems (M) Search a 2D Matrix II
 
+1.
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        if (m==0) return false;
+        int n=matrix[0].size();
+        if (n==0) return false;
+
+        int l=0, r=m*n-1, mid;
+        while (l<=r) {
+            mid=l+(r-l)/2;
+            int row=mid/n, col=mid%n;
+            if (matrix[row][col]==target) return true;
+            else if (matrix[row][col]<target) l=mid+1;
+            else r=mid-1;
+        }
+        return false;
+    }
+};
+
+2.
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
