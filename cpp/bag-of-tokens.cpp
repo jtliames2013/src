898e1c6e587730f4196475289e34608e96d84316
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-16 00:06:35 -0700

    more checkin

---
+++ cpp/bag-of-tokens.cpp
@@ -0,0 +1,62 @@
+948. Bag of Tokens
+Medium
+
+115
+
+105
+
+Favorite
+
+Share
+You have an initial power P, an initial score of 0 points, and a bag of tokens.
+
+Each token can be used at most once, has a value token[i], and has potentially two ways to use it.
+
+If we have at least token[i] power, we may play the token face up, losing token[i] power, and gaining 1 point.
+If we have at least 1 point, we may play the token face down, gaining token[i] power, and losing 1 point.
+Return the largest number of points we can have after playing any number of tokens.
+
+ 
+
+Example 1:
+
+Input: tokens = [100], P = 50
+Output: 0
+Example 2:
+
+Input: tokens = [100,200], P = 150
+Output: 1
+Example 3:
+
+Input: tokens = [100,200,300,400], P = 200
+Output: 2
+ 
+
+Note:
+
+tokens.length <= 1000
+0 <= tokens[i] < 10000
+0 <= P < 10000
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int P) {
+        sort(tokens.begin(), tokens.end());
+        int res=0, points=0, n=tokens.size(), l=0, r=n-1;
+        while (l<=r) {
+            if (P>=tokens[l]) {
+                P-=tokens[l];
+                points++;
+                l++;
+                res=max(res, points);
+            } else if (points>0) {
+                P+=tokens[r];
+                r--;
+                points--;
+            } else {
+                break;
+            }
+        }
+        return res;
+    }
+};
