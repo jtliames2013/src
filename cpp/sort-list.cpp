32d578379c15a8a856b610c7bceaa561f4f5a11e
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-18 08:04:57 -0700

    more checkin

--- cpp/sort-list.cpp
+++ cpp/sort-list.cpp
@@ -22,11 +22,12 @@ Hide Similar Problems (E) Merge Two Sorted Lists (M) Sort Colors (M) Insertion S
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        return mergeSort(head);   
+        return mergeSort(head);
     }
 private:
     ListNode* mergeSort(ListNode* head) {
-        if (head==NULL||head->next==NULL) return head;
+        if (head==NULL) return head;
+        if (head->next==NULL) return head;
         ListNode *one=head, *two=head, *prev=NULL;
         while (two && two->next) {
             prev=one;
@@ -34,11 +35,13 @@ private:
             two=two->next->next;
         }
         prev->next=NULL; //prev can't be NULL
-        ListNode *l1=mergeSort(head);
-        ListNode *l2=mergeSort(one);
-        ListNode *newHead=NULL, *tail=NULL;
+
+        ListNode* l1=mergeSort(head);
+        ListNode* l2=mergeSort(one);
+        ListNode dummy(0);
+        ListNode *tail=&dummy;
         while (l1 && l2) {
-            ListNode *l;
+            ListNode* l;
             if (l1->val<l2->val) {
                 l=l1;
                 l1=l1->next;
@@ -46,16 +49,12 @@ private:
                 l=l2;
                 l2=l2->next;
             }
-            if (newHead==NULL) {
-                newHead=tail=l;
-            } else {
-                tail->next=l;
-                tail=l;
-            }
+            tail->next=l;
+            tail=l;
         }
-        tail->next=l1!=NULL?l1:l2;
+        tail->next=l1?l1:l2;
         
-        return newHead;
+        return dummy.next;
     }
 };
 
