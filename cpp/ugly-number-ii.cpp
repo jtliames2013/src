ce35854ae13ed5b72c06e1e92dd13ddb8edd1efe
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-17 23:56:01 -0700

    more checkin

--- cpp/ugly-number-ii.cpp
+++ cpp/ugly-number-ii.cpp
@@ -31,12 +31,11 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         if (n<=0) return 0;
-        if (n==1) return 1;
-        int i2=0, i3=0, i5=0;
         vector<int> res(n);
         res[0]=1;
-        for (int i=1; i<n; i++) {
-            res[i]=min(min(res[i2]*2, res[i3]*3), res[i5]*5);
+        int i2=0, i3=0, i5=0;
+        for (int i=1; i<n; ++i) {
+            res[i]=min({res[i2]*2, res[i3]*3, res[i5]*5});
             if (res[i]==res[i2]*2) i2++;
             if (res[i]==res[i3]*3) i3++;
             if (res[i]==res[i5]*5) i5++;
@@ -44,4 +43,3 @@ public:
         return res[n-1];
     }
 };
-
