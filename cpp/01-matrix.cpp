6b3c1986f0284cf3e8b400c2e881f1f23b1aad5d
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-18 09:36:37 -0700

    more checkin

--- cpp/01-matrix.cpp
+++ cpp/01-matrix.cpp
@@ -36,6 +36,7 @@ Subscribe to see which companies asked this question.
 
 Hide Tags Depth-first Search Breadth-first Search
 
+1. BFS
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
@@ -43,7 +44,7 @@ public:
         if (m==0)  return matrix;
         int n=matrix[0].size();
         if (n==0) return matrix;
-        
+
         queue<pair<int,int>> q;
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
@@ -61,13 +62,13 @@ public:
             for (int k=0; k<4; k++) {
                 int nr=f.first+delta[k][0];
                 int nc=f.second+delta[k][1];
-                if (nr>=0 && nr<m && nc>=0 && nc<n && matrix[nr][nc]>matrix[f.first][f.second]) {
+                if (nr>=0 && nr<m && nc>=0 && nc<n && matrix[nr][nc]>matrix[f.first][f.second]+1) {
                     q.push({nr,nc});
                     matrix[nr][nc]=matrix[f.first][f.second]+1;
                 }
             }
         }
-        
+
         return matrix;
     }
     
@@ -75,3 +76,50 @@ private:
     const int delta[4][2]={{-1,0}, {1,0}, {0,-1}, {0,1}};    
 };
 
+
+2. optimized DFS
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        if (m==0) return matrix;
+        int n=matrix[0].size();
+        if (n==0) return matrix;
+
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (matrix[i][j]==1 && !hasZeroNeighbor(matrix, i, j, m, n)) matrix[i][j]=INT_MAX;
+            }
+        }
+
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (matrix[i][j]==1) dfs(matrix, i, j, m, n);
+            }
+        }
+
+        return matrix;
+    }
+private:
+    bool hasZeroNeighbor(vector<vector<int>>& matrix, int row, int col, int m, int n) {
+        for (int k=0; k<4; ++k) {
+            int nr=row+delta[k][0];
+            int nc=col+delta[k][1];
+            if (nr>=0 && nr<m && nc>=0 && nc<n && matrix[nr][nc]==0) return true;
+        }
+        return false;
+    }
+
+    void dfs(vector<vector<int>>& matrix, int row, int col, int m, int n) {
+        for (int k=0; k<4; ++k) {
+            int nr=row+delta[k][0];
+            int nc=col+delta[k][1];
+            if (nr>=0 && nr<m && nc>=0 && nc<n && matrix[nr][nc]>matrix[row][col]+1) {
+                matrix[nr][nc]=matrix[row][col]+1;
+                dfs(matrix, nr, nc, m, n);
+            }
+        }
+    }
+
+    const int delta[4][2]={{-1,0}, {1,0}, {0,-1}, {0,1}};
+};
