a628aa7b755d6ff017c29911d2c21c0af32e18f8
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-21 23:35:50 -0700

    more checkin

--- cpp/new-21-game.cpp
+++ cpp/new-21-game.cpp
@@ -31,14 +31,17 @@ The judging time limit has been reduced for this question.
 class Solution {
 public:
     double new21Game(int N, int K, int W) {
-        if (K==0 || N>=K+W) return 1;
+        if (K==0 || N>=K-1+W) return 1;
         double res=0, sum=1;
         vector<double> dp(N+1);
         dp[0]=1;
+        // dp[i]: probability of get points i
+        // dp[i] = sum(last W dp values) / W
         for (int i=1; i<=N; ++i) {
             dp[i]=sum/W;
-            if (i<K) sum+=dp[i]; else res+=dp[i]; 
-            if (i-W>=0) sum-=dp[i-W];
+            if (i<K) sum+=dp[i];
+            else res+=dp[i];
+            if (i>=W) sum-=dp[i-W];
         }
         return res;
     }
