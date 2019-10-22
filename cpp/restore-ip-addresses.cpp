d9aa46ed7565fc670db73b2ad2fb7564e79758d5
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-21 22:27:45 -0700

    more checkin

--- cpp/restore-ip-addresses.cpp
+++ cpp/restore-ip-addresses.cpp
@@ -9,36 +9,32 @@ return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
 
 class Solution {
 public:
-    void dfs(vector<string>& res, string& s, string& ip, int start, int numAddr) {
-        if (numAddr==4) {
-            if (start==s.size()) {
-                res.push_back(ip);
-            }
-            return;
-        }    
-        
-        for (int i=start; i<s.size() && i<=start+2; i++) {
-            int num=0;
-            for (int j=start; j<=i; j++) {
-                num*=10;
-                num+=s[j]-'0';
-            }
-            if (num<=255 && (i==start||s[start]!='0')) {
-                int len=ip.size();
-                if (ip!="") ip+=".";
-                ip+=s.substr(start, i-start+1);
-                dfs(res, s, ip, i+1, numAddr+1);
-                ip.erase(len);
-            }
-        }
-    }
-    
-    vector<string> restoreIpAddresses(string s) {
+    vector<string> restoreIpAddresses(string s) {        
         vector<string> res;
-        string ip;
-        dfs(res, s, ip, 0, 0);
-        
+        string output;
+        dfs(res, output, s, 0, 0);
         return res;
     }
+private:
+    void dfs(vector<string>& res, string& output, string& s, int start, int part) {
+        if (part==4) {
+            if (start==s.size()) {
+                res.push_back(output);
+            }
+            return;
+        }
+        
+        for (int i=start; i<s.size() && i-start+1<=3; ++i) {
+            if (s[start]=='0' && i>start) break;
+            string str=s.substr(start, i-start+1);
+            int num=stoi(str);
+            if (num>255) continue;
+            
+            int len=output.size();
+            if (!output.empty()) output+=".";
+            output+=str;
+            dfs(res, output, s, i+1, part+1);
+            output.erase(len);
+        }
+    }
 };
-
