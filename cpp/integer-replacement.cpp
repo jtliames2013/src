b637254767105839a075549b79f5dd41ddc3868f
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-23 19:11:55 -0700

    more checkin

--- cpp/integer-replacement.cpp
+++ cpp/integer-replacement.cpp
@@ -1,13 +1,9 @@
-397. Integer Replacement
-Medium
-
-249
-
-263
-
-Favorite
-
-Share
+397. Integer Replacement Add to List
+DescriptionHintsSubmissionsSolutions
+Total Accepted: 17728
+Total Submissions: 59800
+Difficulty: Medium
+Contributor: LeetCode
 Given a positive integer n and you can do operations as follow:
 
 If n is even, replace n with n/2.
@@ -24,6 +20,7 @@ Output:
 
 Explanation:
 8 -> 4 -> 2 -> 1
+
 Example 2:
 
 Input:
@@ -36,18 +33,42 @@ Explanation:
 7 -> 8 -> 4 -> 2 -> 1
 or
 7 -> 6 -> 3 -> 2 -> 1
+Subscribe to see which companies asked this question.
+
+Hide Tags Math Bit Manipulation
+
+if a number ends with 01, then certainly decrementing is the way to go. Otherwise, if it ends with 11, then certainly incrementing is at least as good as decrementing 
 
 class Solution {
 public:
     int integerReplacement(int n) {
-        int res=0;
-        long x=n;
-        while (x>1) {
-            if ((x&1)==0) x>>=1;
-            else if (x==3 || ((x>>1)&1)==0) x--;
-            else x++;
-            res++;
+        int count=0;
+        long long num=n;
+        while (num>1) {
+            if ((num & 0x1)==0) {
+                num>>=1;
+            } else if (num==3 || ((num>>1) & 0x1)==0) {
+                num--;
+            } else {
+                num++;
+            }
+            count++;
         }
-        return res;
+        return count;
+    }
+};
+
+2. Recursion
+class Solution {
+public:
+    int dfs(long long num) {
+        if (num==1) return 0;
+        if ((num & 0x1)==0) return dfs(num/2)+1;
+        else if (num==3 || ((num>>1) & 0x1)==0) return dfs(num-1)+1;
+        else return dfs(num+1)+1;
+    }
+
+    int integerReplacement(int n) {
+        return dfs(n);
     }
 };
