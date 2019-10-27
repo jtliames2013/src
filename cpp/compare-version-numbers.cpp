7782dcd25a9a6b28101cb2e2affc3c5d81118e9a
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-27 09:32:00 -0700

    more checkin

--- cpp/compare-version-numbers.cpp
+++ cpp/compare-version-numbers.cpp
@@ -25,29 +25,17 @@ note 1.0.0 equals 1
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        int i1=0, i2=0, n1=version1.size(), n2=version2.size();
-        int num1=0, num2=0;
+        int i1=0, i2=0, n1=version1.size(), n2=version2.size(), num1, num2;
         while (i1<n1 || i2<n2) {
-            num1=0;
-            num2=0;
-            while (i1<n1 && version1[i1]!='.') {
-                num1*=10;
-                num1+=version1[i1]-'0';
-                i1++;
-            }
-            
-            while (i2<n2 && version2[i2]!='.') {
-                num2*=10;
-                num2+=version2[i2]-'0';
-                i2++;
-            }
-            
-            if (num1>num2) return 1;
-            else if (num1<num2) return -1;
+            num1=0, num2=0;
+            while (i1<n1 && version1[i1]!='.') num1=num1*10+version1[i1++]-'0';
+            while (i2<n2 && version2[i2]!='.') num2=num2*10+version2[i2++]-'0';
+            if (num1<num2) return -1;
+            else if (num1>num2) return 1;
             i1++;
             i2++;
         }
+
         return 0;
     }
 };
-
