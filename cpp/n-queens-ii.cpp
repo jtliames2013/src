00f41ef6c8f2a844217e3238dc3d3efbc1f3522e
jtliames2013 <jtli.ames2013@gmail.com>
2019-10-29 09:15:17 -0700

    more checkin

--- cpp/n-queens-ii.cpp
+++ cpp/n-queens-ii.cpp
@@ -9,36 +9,34 @@ Hide Similar Problems (H) N-Queens
 
 class Solution {
 public:
-	bool isValid(int row, int col, vector<int>& pos) {
-		for (int i=0; i<pos.size(); i++) {
-			if (pos[i]==col || abs(row-i)==abs(pos[i]-col)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	void solve(int n, int row, vector<int>& pos, int& res) {
-		if (row==n) {
-			res++;
-			return;
-		}
-
-		for (int col=0; col<n; col++) {
-			if (isValid(row, col, pos)) {
-				pos.push_back(col);
-				solve(n, row+1, pos, res);
-				pos.pop_back();
-			}
-		}
-	}
-
     int totalNQueens(int n) {
-    	int res=0;
-    	vector<int> pos;
-    	solve(n, 0, pos, res);
+        int res=0;
+        vector<int> pos;
+        dfs(res, pos, n, 0);
 
-    	return res;
+        return res;
+    }
+private:
+    void dfs(int& res, vector<int>& pos, int n, int start) {
+        if (start==n) {
+            res++;
+            return;
+        }
+
+        for (int col=0; col<n; ++col) {
+            if (isValid(pos, col)) {
+                pos.push_back(col);
+                dfs(res, pos, n, start+1);
+                pos.pop_back();
+            }
+        }
+    }
+
+    bool isValid(vector<int>& pos, int col) {
+        int row=pos.size();
+        for (int i=0; i<pos.size(); ++i) {
+            if (pos[i]==col || abs(pos[i]-col)==row-i) return false;
+        }
+        return true;
     }
 };
-
