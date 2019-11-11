5d22429722cd15e7aba16c6a12eb80715dce935c
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-11 11:07:23 -0800

    more checkin

---
+++ cpp/maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,78 @@
+1235. Maximum Profit in Job Scheduling
+Hard
+
+161
+
+1
+
+Favorite
+
+Share
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime , endTime and profit arrays, you need to output the maximum profit you can take such that there are no 2 jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+ 
+
+Example 1:
+
+
+
+Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+Output: 120
+Explanation: The subset chosen is the first and fourth job. 
+Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+Example 2:
+
+
+
+
+Input: startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60]
+Output: 150
+Explanation: The subset chosen is the first, fourth and fifth job. 
+Profit obtained 150 = 20 + 70 + 60.
+Example 3:
+
+
+
+Input: startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4]
+Output: 6
+ 
+
+Constraints:
+
+1 <= startTime.length == endTime.length == profit.length <= 5 * 10^4
+1 <= startTime[i] < endTime[i] <= 10^9
+1 <= profit[i] <= 10^4
+
+dp[time] = profit means that within the first time duration,
+we cam make at most profit money.
+Intial dp[0] = 0, as we make profit = 0 at time = 0.
+
+For each job = [s, e, p], where s,e,p are its start time, end time and profit,
+Then the logic is similar to the knapsack problem.
+If we don't do this job, nothing will be changed.
+If we do this job, binary search in the dp to find the largest profit we can make before start time s.
+So we also know the maximum current profit that we can make doing this job.
+
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n=startTime.size();
+        vector<vector<int>> jobs(n);
+        for (int i=0; i<n; ++i) jobs[i]={startTime[i], endTime[i], profit[i]};        
+        sort (jobs.begin(), jobs.end(), [](vector<int>& a, vector<int>& b){ return a[1]<b[1]; });
+
+        // Since we sort by endTime, the last element in map has the current max profit
+        map<int, int> mp;
+        mp[0]=0;
+        for (auto& job:jobs) {
+            int profit=prev(mp.upper_bound(job[0]))->second+job[2];
+            if (profit>mp.rbegin()->second) mp[job[1]]=profit;
+        }
+        return mp.rbegin()->second;
+    }
+};
+
