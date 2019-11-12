e2ef9f73f768941120645808344bef610de6bec1
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-12 08:16:30 -0800

    more checkin

--- cpp/smallest-rotation-with-highest-score.cpp
+++ cpp/smallest-rotation-with-highest-score.cpp
@@ -34,10 +34,10 @@ class Solution {
 public:
     int bestRotation(vector<int>& A) {
         int n=A.size();
-        vector<int> change(n);
-        for (int i=0; i<n; i++) change[(i-A[i]+1+n)%n]--;
-        for (int i=1; i<n; i++) change[i]+=change[i-1]+1;
-        return distance(change.begin(), max_element(change.begin(), change.end()));        
+        vector<int> change(n, 1);
+        for (int i=0; i<n; ++i) change[(i-A[i]+1+n)%n]--;
+        for (int i=1; i<n; ++i) change[i]+=change[i-1];
+        return distance(change.begin(), max_element(change.begin(), change.end()));
     }
 };
 
