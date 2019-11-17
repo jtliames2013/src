abe515a3dac4ea2f9cce495aa90858446f379de1
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-17 11:51:39 -0800

    more checkin

--- cpp/sudoku-solver.cpp
+++ cpp/sudoku-solver.cpp
@@ -23,63 +23,64 @@ Hide Similar Problems (M) Valid Sudoku
 
 class Solution {
 public:
-	bool isValid(vector<vector<char> >& board, int row, int col) {
-		for (int c=0; c<9; c++) {
-			if (c!=col && board[row][c] == board[row][col]) return false;
-		}
-
-		for (int r=0; r<9; r++) {
-			if (r!=row && board[r][col] == board[row][col]) return false;
-		}
-
-		int br=row/3;
-		int bc=col/3;
-
-		for (int i=0; i<3; i++) {
-			for (int j=0; j<3; j++) {
-				if ((i+br*3 != row || j+bc*3 != col) && board[i+br*3][j+bc*3]==board[row][col]) return false;
-			}
-		}
-
-		return true;
+    bool isValid(vector<vector<char> >& board, int row, int col) {
+	for (int c=0; c<9; c++) {
+	    if (c!=col && board[row][c] == board[row][col]) return false;
 	}
 
-	bool solveSudoku(vector<vector<char> >& board, int row, int col) {
-		if (row==9) return true;
-
-		int nextrow, nextcol;
-		if (col==8) {
-			nextrow=row+1;
-			nextcol=0;
-		} else {
-			nextrow=row;
-			nextcol=col+1;
-		}
-
-		if (board[row][col]=='.') {
-			for (int i=1; i<=9; i++) {
-				board[row][col]='0'+i;
-				if (isValid(board, row, col)) {
-					if (solveSudoku(board, nextrow, nextcol)) {
-						return true;
-					}
-				}
-			}
-
-			board[row][col]='.';
-			return false;
-		} else {
-			return solveSudoku(board, nextrow, nextcol);
-		}
+	for (int r=0; r<9; r++) {
+	    if (r!=row && board[r][col] == board[row][col]) return false;
 	}
+
+	int rb=row/3;
+	int cb=col/3;
+	for (int i=0; i<3; i++) {
+	    for (int j=0; j<3; j++) {
+                int r=rb*3+i, c=cb*3+j;
+		if ((r != row || c != col) && board[r][c]==board[row][col]) return false;
+	    }
+	}
+
+	return true;
+    }
+
+    bool solveSudoku(vector<vector<char> >& board, int row, int col) {
+	if (row==9) return true;
+
+	int nextrow, nextcol;
+	if (col==8) {
+	    nextrow=row+1;
+	    nextcol=0;
+	} else {
+	    nextrow=row;
+	    nextcol=col+1;
+	}
+
+	if (board[row][col]=='.') {
+	    for (int i=1; i<=9; i++) {
+		board[row][col]='0'+i;
+		if (isValid(board, row, col)) {
+		    if (solveSudoku(board, nextrow, nextcol)) {
+			return true;
+		    }
+		}
+	    }
+
+	    board[row][col]='.';
+	    return false;
+	} else {
+	    return solveSudoku(board, nextrow, nextcol);
+	}
+    }
 
     void solveSudoku(vector<vector<char>>& board) {
-    	if (board.size()!=9) return;
-    	if (board[0].size()!=9) return;
+	if (board.size()!=9) return;
+	if (board[0].size()!=9) return;
 
-    	solveSudoku(board, 0, 0);
+	solveSudoku(board, 0, 0);
     }
 };
+
 
 2.
 class Solution {
