e6e3e6a1a11ace1edf7a30b1bc901572c39416ff
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-19 22:43:28 -0800

    more checkin

--- cpp/k-similar-strings.cpp
+++ cpp/k-similar-strings.cpp
@@ -25,3 +25,39 @@ Note:
 1 <= A.length == B.length <= 20
 A and B contain only lowercase letters from the set {'a', 'b', 'c', 'd', 'e', 'f'}
 
+class Solution {
+public:
+    int kSimilarity(string A, string B) {
+        if (A==B) return 0;
+        int res=0, n=A.size();
+        queue<string> q;
+        unordered_set<string> visited;
+        q.push(A);
+        visited.insert(A);
+        
+        while (!q.empty()) {
+            int size=q.size();
+            for (int i=0; i<size; ++i) {
+                auto f=q.front();
+                q.pop();
+                
+                int j=0;
+                while (j<n && f[j]==B[j]) j++;
+                for (int k=j+1; k<n; ++k) {
+                    if (f[k]==B[k] || f[k]!=B[j]) continue;
+                    string next=f;
+                    swap(next[j], next[k]);
+                    if (next==B) return res+1;
+                    if (visited.find(next)==visited.end()) {
+                        q.push(next);
+                        visited.insert(next);
+                    }
+                }
+            }
+
+            res++;
+        }
+        return -1;
+    }
+};
+
