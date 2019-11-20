f78d693eb43fd704252aa54ac90d494f371dc2b0
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-20 10:39:26 -0800

    more checkin

---
+++ cpp/stamping-the-sequence.cpp
@@ -0,0 +1,83 @@
+936. Stamping The Sequence
+Hard
+
+113
+
+32
+
+Favorite
+
+Share
+You want to form a target string of lowercase letters.
+
+At the beginning, your sequence is target.length '?' marks.  You also have a stamp of lowercase letters.
+
+On each turn, you may place the stamp over the sequence, and replace every letter in the sequence with the corresponding letter from the stamp.  You can make up to 10 * target.length turns.
+
+For example, if the initial sequence is "?????", and your stamp is "abc",  then you may make "abc??", "?abc?", "??abc" in the first turn.  (Note that the stamp must be fully contained in the boundaries of the sequence in order to stamp.)
+
+If the sequence is possible to stamp, then return an array of the index of the left-most letter being stamped at each turn.  If the sequence is not possible to stamp, return an empty array.
+
+For example, if the sequence is "ababc", and the stamp is "abc", then we could return the answer [0, 2], corresponding to the moves "?????" -> "abc??" -> "ababc".
+
+Also, if the sequence is possible to stamp, it is guaranteed it is possible to stamp within 10 * target.length moves.  Any answers specifying more than this number of moves will not be accepted.
+
+ 
+
+Example 1:
+
+Input: stamp = "abc", target = "ababc"
+Output: [0,2]
+([1,0,2] would also be accepted as an answer, as well as some other answers.)
+Example 2:
+
+Input: stamp = "abca", target = "aabcaca"
+Output: [3,0,1]
+ 
+
+Note:
+
+1 <= stamp.length <= target.length <= 1000
+stamp and target only contain lowercase letters.
+
+Greedy, convert reversely from target to original string
+
+class Solution {
+public:
+    vector<int> movesToStamp(string stamp, string target) {
+        vector<int> res;
+        int m=stamp.size(), n=target.size();
+        
+        bool changed;
+        do {
+            changed=false;
+            for (int i=0; i<=n-m; ++i) {
+                changed|=match(res, stamp, target, m, n, i);
+            }
+        } while (changed);
+    
+        for (int i=0; i<n; ++i) {
+            if (target[i]!='?') return vector<int>();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+private:
+    bool match(vector<int>& res, string& stamp, string& target, int m, int n, int i) {
+        bool changed=false;        
+        for (int j=0; j<m; ++j) {
+            if (target[i+j]=='?') continue;
+            if (target[i+j]!=stamp[j]) return false;
+            changed=true;
+        }
+        
+        if (changed) {
+            fill(target.begin()+i, target.begin()+i+m, '?');
+            res.push_back(i);
+        }
+
+        return changed;
+    }
+};
+
+
