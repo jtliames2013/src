093f702ea0bf95960044769e0c4efa9825bcd3ac
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-23 14:47:59 -0800

    more checkin

--- cpp/basic-calculator.cpp
+++ cpp/basic-calculator.cpp
@@ -20,10 +20,12 @@ digit: it should be one digit from the current number
 '(': push the previous result and the sign into the stack, set result to 0, just calculate the new result within the parenthesis.
 ')': pop out the top two numbers from stack, first one is the sign before this pair of parenthesis, second is the temporary result before this pair of parenthesis. We add them together.
 Finally if there is only one number, from the above solution, we haven't add the number to the result, so we do a check see if the number is zero.
+
+1.
 class Solution {
 public:
     int calculate(string s) {       
-        int res=0, num=0, sign=1;
+        long res=0, num=0, sign=1;
         stack<int> results, signs; 
         for (int i=0; i<s.size(); i++) {
             if (isdigit(s[i])) {
