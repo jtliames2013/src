f5a836d02e42ec75f523b481c81133a11a2e3105
jtliames2013 <jtli.ames2013@gmail.com>
2019-11-24 19:04:15 -0800

    more checkin

--- cpp/maximum-gap.cpp
+++ cpp/maximum-gap.cpp
@@ -18,36 +18,36 @@ Hide Tags Sort
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-    	 int n=nums.size();
-    	 if (n<2) return 0;
-    	 int maxGap;
+        int n=nums.size();
+        if (n<2) return 0;
+        int maxGap, minVal=nums[0], maxVal=nums[0];
 
-    	 int minVal=nums[0], maxVal=nums[0];
-    	 for (int i=1; i<n; i++) {
-    		 minVal=min(minVal, nums[i]);
-    		 maxVal=max(maxVal, nums[i]);
-    	 }
+        for (int i=1; i<n; ++i) {
+            minVal=min(minVal, nums[i]);
+            maxVal=max(maxVal, nums[i]);
+        }
 
-    	 int bucketRange=(maxVal-minVal-1)/(n-1)+1;
-    	 int bucketLen=(maxVal-minVal)/bucketRange+1;
-    	 vector<int> bucketMin(bucketLen, INT_MAX);
-    	 vector<int> bucketMax(bucketLen, 0);
-    	 for (int i=0; i<nums.size(); i++) {
-    		 int idx=(nums[i]-minVal)/bucketRange;
-    		 bucketMin[idx]=min(bucketMin[idx], nums[i]);
-    		 bucketMax[idx]=max(bucketMax[idx], nums[i]);
-    	 }
+        // make bucket length such that it must be less than maximum gap
+        // use +1 instead of ceiling in case maxVal equals to minVal
+        int bucketLen=(maxVal-minVal)/(n-1)+1;
+        int bucketNum=(maxVal-minVal)/bucketLen+1;
+        vector<int> bucketMin(bucketNum, INT_MAX), bucketMax(bucketNum);
+        for (int i=0; i<n; ++i) {
+            int idx=(nums[i]-minVal)/bucketLen;
+            bucketMin[idx]=min(bucketMin[idx], nums[i]);
+            bucketMax[idx]=max(bucketMax[idx], nums[i]);
+        }
 
-    	 maxGap = bucketMax[0]-bucketMin[0];
-    	 int prev=0;
-    	 for (int i=1; i<bucketLen; i++) {
-    		 if (bucketMin[i]<INT_MAX) {
-				 maxGap=max(maxGap, bucketMin[i]-bucketMax[prev]);
-				 prev=i;
-    		 }
-    	 }
-
-    	 return maxGap;
+        maxGap=bucketMax[0]-bucketMin[0];
+        int prev=0;
+        for (int i=1; i<bucketNum; ++i) {
+            if (bucketMin[i]!=INT_MAX) {
+                // bucket not empty
+                maxGap=max({maxGap, bucketMin[i]-bucketMax[prev]});
+                prev=i;
+            }
+        }
+        return maxGap;
     }
 };
 
