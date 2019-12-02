cfa1ea7977c1437b4e0c8580216c20c2f90169b2
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-02 10:25:39 -0800

    more checkin

--- cpp/median-of-two-sorted-arrays.cpp
+++ cpp/median-of-two-sorted-arrays.cpp
@@ -24,39 +24,36 @@ Hide Tags Binary Search Array Divide and Conquer
 
 class Solution {
 public:
-	int findKthElement(vector<int>& nums1, int s1, int e1, vector<int>& nums2, int s2, int e2, int k) {
-		if (s1>e1) return nums2[s2+k];
-		if (s2>e2) return nums1[s1+k];
-
-		int m1=(s1+e1)/2;
-		int m2=(s2+e2)/2;
-		if (nums1[m1]>nums2[m2]) {
-			if (k>(m1-s1+m2-s2)) {
-				return findKthElement(nums1, s1, e1, nums2, m2+1, e2, k-(m2-s2)-1);
-			} else {
-				return findKthElement(nums1, s1, m1-1, nums2, s2, e2, k);
-			}
-		} else {
-			if (k>(m1-s1+m2-s2)) {
-				return findKthElement(nums1, m1+1, e1, nums2, s2, e2, k-(m1-s1)-1);
-			} else {
-				return findKthElement(nums1, s1, e1, nums2, s2, m2-1, k);
-			}
-		}
-	}
-
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    	int size1=nums1.size();
-    	int size2=nums2.size();
-    	int m=(size1+size2)/2;
+        int n1=nums1.size(), n2=nums2.size(), m=(n1+n2)/2;
 
-    	if ((size1+size2)%2==0) {
-    		double m1 = findKthElement(nums1, 0, size1-1, nums2, 0, size2-1, m-1);
-    		double m2 = findKthElement(nums1, 0, size1-1, nums2, 0, size2-1, m);
-    		return 0.5 * (m1+m2);
-    	} else {
-    		return findKthElement(nums1, 0, size1-1, nums2, 0, size2-1, m);
-    	}
+        if ((n1+n2)%2==0) {
+            double m1=findKthElement(nums1, 0, n1-1, nums2, 0, n2-1, m-1);
+            double m2=findKthElement(nums1, 0, n1-1, nums2, 0, n2-1, m);
+            return (m1+m2)/2.0;
+        } else {
+            return findKthElement(nums1, 0, n1-1, nums2, 0, n2-1, m);
+        }
+    }
+private:
+    int findKthElement(vector<int>& nums1, int s1, int e1, vector<int>& nums2, int s2, int e2, int k) {
+        if (s1>e1) return nums2[s2+k];
+        if (s2>e2) return nums1[s1+k];
+
+        int m1=(s1+e1)/2, m2=(s2+e2)/2;
+        if (nums1[m1]>nums2[m2]) {
+            if (k>(m1-s1+m2-s2)) {
+                return findKthElement(nums1, s1, e1, nums2, m2+1, e2, k-(m2-s2)-1);
+            } else {
+                return findKthElement(nums1, s1, m1-1, nums2, s2, e2, k);
+            }
+        } else {
+            if (k>(m1-s1+m2-s2)) {
+                return findKthElement(nums1, m1+1, e1, nums2, s2, e2, k-(m1-s1)-1);
+            } else {
+                return findKthElement(nums1, s1, e1, nums2, s2, m2-1, k);
+            }
+        }
     }
 };
 
