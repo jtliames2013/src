6b299ada0e4200200e9511ad6287e4839d07c7fd
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-02 08:30:34 -0800

    more checkin

--- cpp/interleaving-string.cpp
+++ cpp/interleaving-string.cpp
@@ -23,13 +23,13 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n1=s1.size(), n2=s2.size(), n3=s3.size();
         if (n1+n2!=n3) return false;
-        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1));
         for (int i=0; i<=n1; i++) {
             for (int j=0; j<=n2; j++) {
                 if (i==0 && j==0) dp[i][j]=true;
                 else {
-                    if (i>0) dp[i][j]=(dp[i][j] || (dp[i-1][j]==true && s3[i+j-1]==s1[i-1]));
-                    if (j>0) dp[i][j]=(dp[i][j] || (dp[i][j-1]==true && s3[i+j-1]==s2[j-1]));
+                    if (i>0) dp[i][j]=(dp[i][j] || (dp[i-1][j] && s3[i+j-1]==s1[i-1]));
+                    if (j>0) dp[i][j]=(dp[i][j] || (dp[i][j-1] && s3[i+j-1]==s2[j-1]));
                 }
             }
         }
