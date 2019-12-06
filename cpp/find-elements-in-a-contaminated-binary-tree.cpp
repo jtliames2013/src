981457a558bcde5b129e757279b9034423185e76
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-05 21:24:56 -0800

    more checkin

---
+++ cpp/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,127 @@
+1261. Find Elements in a Contaminated Binary Tree
+Medium
+
+64
+
+14
+
+Favorite
+
+Share
+Given a binary tree with the following rules:
+
+root.val == 0
+If treeNode.val == x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+If treeNode.val == x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+You need to first recover the binary tree and then implement the FindElements class:
+
+FindElements(TreeNode* root) Initializes the object with a contamined binary tree, you need to recover it first.
+bool find(int target) Return if the target value exists in the recovered binary tree.
+ 
+
+Example 1:
+
+
+
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]); 
+findElements.find(1); // return False 
+findElements.find(2); // return True 
+Example 2:
+
+
+
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+Example 3:
+
+
+
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+ 
+
+Constraints:
+
+TreeNode.val == -1
+The height of the binary tree is less than or equal to 20
+The total number of nodes is between [1, 10^4]
+Total calls of find() is between [1, 10^4]
+0 <= target <= 10^6
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class FindElements {
+public:
+    FindElements(TreeNode* root) {
+        this->root=root;
+        dfs(root, 1);
+    }
+    
+    bool find(int target) {
+        TreeNode* n=root;
+        target++;
+        vector<int> stk;
+        int x=target;
+        while (x) {
+            stk.push_back(x&1);
+            x>>=1;
+        }
+
+        while (n) {
+            if (n->val==target) return true;
+            stk.pop_back();
+            if (stk.empty()) return false;
+            if (stk.back()==0) n=n->left;
+            else n=n->right;
+        }
+        return false;
+    }
+private:    
+    void dfs(TreeNode* root, int val) {
+        if (root==NULL) return;
+        root->val=val;
+        dfs(root->left, 2*val);
+        dfs(root->right, 2*val+1);
+    }
+        
+    TreeNode* root;
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
+
