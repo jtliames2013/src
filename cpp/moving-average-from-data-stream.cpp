fd3c20e40981bb0eef9800905297fe9d0e6eb2d6
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-08 10:36:18 -0800

    more checkin

--- cpp/moving-average-from-data-stream.cpp
+++ cpp/moving-average-from-data-stream.cpp
@@ -1,15 +1,42 @@
-46. Moving Average from Data Stream 
+346. Moving Average from Data Stream
+Easy
+
+398
+
+46
+
+Favorite
+
+Share
 Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
-For example,
+Example:
+
 MovingAverage m = new MovingAverage(3);
 m.next(1) = 1
 m.next(10) = (1 + 10) / 2
 m.next(3) = (1 + 10 + 3) / 3
 m.next(5) = (10 + 3 + 5) / 3
-Hide Company Tags Google
-Hide Tags Design Queue
 
+0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+
+Amazon
+|
+4
+
+Microsoft
+|
+2
+
+Google
+|
+2
+
+Uber
+|
+2
+
+1. deque
 class MovingAverage {
 public:
     /** Initialize your data structure here. */
@@ -28,14 +55,40 @@ public:
         return sum/dq.size();
     }
 private:
-    deque<int> dq;
     int size;
     double sum;
+    deque<int> dq;
 };
 
 /**
  * Your MovingAverage object will be instantiated and called as such:
- * MovingAverage obj = new MovingAverage(size);
- * double param_1 = obj.next(val);
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
  */
+
+2. circular array
+class MovingAverage {
+public:
+    /** Initialize your data structure here. */
+    MovingAverage(int size) {
+        this->size=size;
+        count=tail=sum=0;
+        data.resize(size);
+    }
+
+    double next(int val) {
+        if (count<size) count++;
+        sum-=data[tail];
+        sum+=val;
+        data[tail]=val;
+        tail=(tail+1)%size;
+        return sum/count;
+    }
+private:
+    int size;
+    int count;
+    int tail;
+    double sum;
+    vector<int> data;
+};
 
