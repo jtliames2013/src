d9d7c17641f9c08bafa27dfad7b4a590b5c4758c
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-08 23:42:43 -0800

    more checkin

--- cpp/two-sum-iii-data-structure-design.cpp
+++ cpp/two-sum-iii-data-structure-design.cpp
@@ -1,15 +1,32 @@
 170. Two Sum III - Data structure design
-DescriptionHintsSubmissionsSolutions
-Discuss   Editorial Solution Pick One
+Easy
+
+228
+
+189
+
+Favorite
+
+Share
 Design and implement a TwoSum class. It should support the following operations: add and find.
 
 add - Add the number to an internal data structure.
 find - Find if there exists any pair of numbers which sum is equal to the value.
 
-For example,
+Example 1:
+
 add(1); add(3); add(5);
 find(4) -> true
 find(7) -> false
+Example 2:
+
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+
+LinkedIn
+|
+4
 
 class TwoSum {
 public:
