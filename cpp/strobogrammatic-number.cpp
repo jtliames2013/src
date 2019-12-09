e30efa5e2455f8f9718bc762cbe6ad77ff2c650e
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-08 21:15:10 -0800

    more checkin

--- cpp/strobogrammatic-number.cpp
+++ cpp/strobogrammatic-number.cpp
@@ -1,16 +1,41 @@
 246. Strobogrammatic Number
-DescriptionHintsSubmissionsSolutions
-Discuss   Editorial Solution Pick One
+Easy
+
+157
+
+382
+
+Favorite
+
+Share
 A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
 
 Write a function to determine if a number is strobogrammatic. The number is represented as a string.
 
-For example, the numbers "69", "88", and "818" are all strobogrammatic.
+Example 1:
+
+Input:  "69"
+Output: true
+Example 2:
+
+Input:  "88"
+Output: true
+Example 3:
+
+Input:  "962"
+Output: false
+
+Facebook
+|
+4
+
+Google
+|
+2
 
 class Solution {
 public:
     bool isStrobogrammatic(string num) {
-        unordered_map<char,char> mp={{'0','0'},{'1','1'},{'8','8'},{'6','9'},{'9','6'}};
         int n=num.size(), l=0, r=n-1;
         while (l<=r) {
             if (mp.find(num[l])==mp.end() || mp[num[l]]!=num[r]) return false;
@@ -19,5 +44,7 @@ public:
         }
         return true;
     }
+private:
+    unordered_map<char,char> mp={{'0','0'},{'1','1'},{'8','8'},{'6','9'},{'9','6'}};
 };
 
