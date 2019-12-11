40c664e85cc59cf1bd2ffd1396f6bf8f134c8ae1
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-10 21:24:50 -0800

    more checkin

--- cpp/range-addition.cpp
+++ cpp/range-addition.cpp
@@ -46,8 +46,9 @@ The optimal time complexity is O(k + n) and uses O(1) extra space.
 Credits:
 Special thanks to @vinod23 for adding this problem and creating all test cases.
 
-Hide Company Tags Google
-Hide Tags Array
+Google
+|
+4
 
 这道题的提示说了我们肯定不能把范围内的所有数字都更新，而是只更新开头结尾两个数字就行了，那么我们的做法就是在开头坐标startIndex位置加上inc，而在结束位置加1的地方加上-inc，那么根据题目中的例子，我们可以得到一个数组，nums = {-2, 2, 3, 2, -2, -3}，然后我们发现对其做累加和就是我们要求的结果result = {-2, 0, 3, 5, 3}
 
@@ -60,8 +61,8 @@ public:
             res[u[1]+1]-=u[2];
         }
         
-        for (int i=1; i<=length; i++) res[i]+=res[i-1];
         res.pop_back();
+        for (int i=1; i<length; i++) res[i]+=res[i-1];
         return res;
     }
 };
