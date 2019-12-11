10c56394b9610117a10f6138ffec1b5e69db56ff
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-10 21:08:35 -0800

    more checkin

--- cpp/wiggle-sort.cpp
+++ cpp/wiggle-sort.cpp
@@ -1,11 +1,23 @@
 280. Wiggle Sort
- Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+Medium
 
-For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+490
 
-Hide Company Tags Google
-Hide Tags Array Sort
-Hide Similar Problems (M) Sort Colors (M) Wiggle Sort II
+50
+
+Favorite
+
+Share
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+Example:
+
+Input: nums = [3,5,2,1,6,4]
+Output: One possible answer is [3,5,1,6,2,4]
+
+Airbnb
+|
+2
 
 如果i是奇数，nums[i] >= nums[i - 1]
 如果i是偶数，nums[i] <= nums[i - 1]
