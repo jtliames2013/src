1c00a91e2076cad712632b484996b3e2354fe4ed
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-11 23:40:10 -0800

    more checkin

--- cpp/plus-one-linked-list.cpp
+++ cpp/plus-one-linked-list.cpp
@@ -1,17 +1,27 @@
-369. Plus One Linked List 
-Given a non-negative number represented as a singly linked list of digits, plus one to the number.
+369. Plus One Linked List
+Medium
+
+358
+
+16
+
+Favorite
+
+Share
+Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
 
 The digits are stored such that the most significant digit is at the head of the list.
 
-Example:
-Input:
-1->2->3
+Example :
 
-Output:
-1->2->4
-Hide Company Tags Google
-Hide Tags Linked List
-Hide Similar Problems (E) Plus One
+Input: [1,2,3]
+Output: [1,2,4]
+
+Google
+|
+2
 
 /**
  * Definition for singly-linked list.
@@ -24,20 +34,20 @@ Hide Similar Problems (E) Plus One
 class Solution {
 public:
     ListNode* plusOne(ListNode* head) {
-        ListNode *dummy=new ListNode(0);
+        ListNode *dummy=new ListNode(0), *curr=dummy, *p=dummy;
         dummy->next=head;
-        ListNode *prev=dummy, *curr=head, *p=dummy;
         while (curr) {
             if (curr->val!=9) p=curr;
-            prev=curr;
             curr=curr->next;
         }
+        
         p->val++;
         p=p->next;
         while (p) {
             p->val=0;
             p=p->next;
         }
+        
         if (dummy->val==0) return dummy->next;
         return dummy;
     }
