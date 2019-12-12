5fafe5864b4111f4743483b1d67f5a663c547220
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-11 23:59:11 -0800

    more checkin

--- cpp/zigzag-iterator.cpp
+++ cpp/zigzag-iterator.cpp
@@ -1,39 +1,52 @@
-281. Zigzag Iterator 
- Given two 1d vectors, implement an iterator to return their elements alternately.
+281. Zigzag Iterator
+Medium
 
-For example, given two 1d vectors:
+309
 
-v1 = [1, 2]
-v2 = [3, 4, 5, 6]
+20
 
-By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+Favorite
 
-Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?
+Share
+Given two 1d vectors, implement an iterator to return their elements alternately.
 
-Clarification for the follow up question - Update (2015-09-18):
-The "Zigzag" order is not clearly defined and is ambiguous for k > 2 cases. If "Zigzag" does not look right to you, replace "Zigzag" with "Cyclic". For example, given the following input:
+ 
 
+Example:
+
+Input:
+v1 = [1,2]
+v2 = [3,4,5,6] 
+Output: [1,3,2,4,5,6]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,3,2,4,5,6].
+ 
+
+Follow up:
+
+What if you are given k 1d vectors? How well can your code be extended to such cases?
+
+Clarification for the follow up question:
+The "Zigzag" order is not clearly defined and is ambiguous for k > 2 cases. If "Zigzag" does not look right to you, replace "Zigzag" with "Cyclic". For example:
+
+Input:
 [1,2,3]
 [4,5,6,7]
 [8,9]
 
-It should return [1,4,8,2,5,9,3,6,7]. 
+Output: [1,4,8,2,5,9,3,6,7].
 
+Google
+|
+2
+
+1.
 class ZigzagIterator {
 public:
-    void tryAdvance() {
-        int curr=row;
-        do {
-            if (col[row]<vec2d[row].size()) break;
-            row=(row+1)%vec2d.size();
-        } while (curr!=row);
-    }
-    
     ZigzagIterator(vector<int>& v1, vector<int>& v2) {
         vec2d.push_back(v1);
         vec2d.push_back(v2);
         row=0;
-        col.resize(2,0);
+        col.resize(vec2d.size());
         tryAdvance();
     }
 
@@ -50,6 +63,14 @@ public:
         return col[row]<vec2d[row].size();
     }
 private:
+    void tryAdvance() {
+        int curr=row;
+        do {
+            if (col[row]<vec2d[row].size()) break;
+            row=(row+1)%vec2d.size();
+        } while (curr!=row);
+    }
+
     vector<vector<int>> vec2d;
     int row;
     vector<int> col;
