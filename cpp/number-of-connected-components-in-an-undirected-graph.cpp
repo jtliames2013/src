ecd89b3ab51d251e34296dfb4565fdefdc6176e0
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-14 22:08:28 -0800

    more checkin

--- cpp/number-of-connected-components-in-an-undirected-graph.cpp
+++ cpp/number-of-connected-components-in-an-undirected-graph.cpp
@@ -20,6 +20,14 @@ Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]], return 1.
 Note:
 You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges. 
 
+Amazon
+|
+5
+
+Microsoft
+|
+2
+
 1. BFS
 class Solution {
 public:
