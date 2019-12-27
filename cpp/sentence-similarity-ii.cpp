153de78037689c39fd8440ec02eedac9d5024e4b
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-27 11:08:49 -0800

    more checkin

--- cpp/sentence-similarity-ii.cpp
+++ cpp/sentence-similarity-ii.cpp
@@ -20,3 +20,42 @@ The length of pairs will not exceed 2000.
 The length of each pairs[i] will be 2.
 The length of each words[i] and pairs[i][j] will be in the range [1, 20].
 
+Google
+|
+3
+
+Amazon
+|
+2
+
+Bloomberg
+|
+2
+
+class Solution {
+public:
+    bool areSentencesSimilarTwo(vector<string>& words1, vector<string>& words2, vector<vector<string>>& pairs) {
+        int n1=words1.size(), n2=words2.size();
+        if (n1!=n2) return false;
+        
+        unordered_map<string, string> root;
+        for (auto& p:pairs) {
+            string p1=find(root, p[0]);
+            string p2=find(root, p[1]);
+            if (p1!=p2) root[p1]=p2;
+        }
+        
+        for (int i=0; i<n1; ++i) {
+            if (words1[i]==words2[i]) continue;
+            if (find(root, words1[i])!=find(root, words2[i])) return false;
+        }
+        return true;
+    }
+private:
+    string find(unordered_map<string, string>& root, string s) {
+        if (root.find(s)==root.end()) root[s]=s;
+        while (root[s]!=s) s=root[s];
+        return s;
+    }
+};
+
