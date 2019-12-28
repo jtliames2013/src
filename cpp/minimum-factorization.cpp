8ecb38731d1fc40b85b915d8adae7de085ee5cce
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-28 11:54:11 -0800

    more checkin

--- cpp/minimum-factorization.cpp
+++ cpp/minimum-factorization.cpp
@@ -32,6 +32,10 @@ Case 1: n < 10 When n is smaller than n, the output is always n+10. For example 
 Case 2: n >= 10 Find all factors of n which are between 2 and 9 (both inclusive). The idea is to start searching from 9 so that the number of digits in result are minimized. For example 9 is preferred over 33 and 8 is preferred over 24.
 Store all found factors in an array. The array would contain digits in non-increasing order, so finally print the array in reverse order.
 
+Tencent
+|
+LeetCode
+
 class Solution {
 public:
     int smallestFactorization(int a) {
