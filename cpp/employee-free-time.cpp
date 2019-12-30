87124871ad563c1ff49b471b443737bb03877c32
jtliames2013 <jtli.ames2013@gmail.com>
2019-12-29 16:33:28 -0800

    more checkin

--- cpp/employee-free-time.cpp
+++ cpp/employee-free-time.cpp
@@ -26,36 +26,81 @@ Note:
 schedule and schedule[i] are lists with lengths in range [1, 50].
 0 <= schedule[i].start < schedule[i].end <= 10^8.
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+Pinterest
+|
+11
+
+Amazon
+|
+7
+
+Airbnb
+|
+3
+
+DoorDash
+|
+3
+
+Uber
+|
+2
+
+Google
+|
+2
+
+Microsoft
+|
+2
+
+Intuit
+|
+2
+
+Bloomberg
+|
+2
+
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
 class Solution {
 public:
-    vector<Interval> employeeFreeTime(vector<vector<Interval>>& schedule) {
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
         vector<Interval> res;
-        map<int,int> points;
-        for (auto s:schedule) {
-            for (auto i:s) {
+        map<int, int> points;
+        for (auto& s:schedule) {
+            for (auto& i:s) {
                 points[i.start]++;
                 points[i.end]--;
             }
         }
-        int cnt=0, prev=-1, prevCnt=-1;
-        for (auto p:points) {
+
+        int cnt=0, start=-1;
+        for(auto& p:points) {
             cnt+=p.second;
-            if (cnt==0) {
-                prev=p.first;
-            } else {
-                if (prevCnt==0) res.push_back(Interval(prev, p.first));
+            if (cnt==0) start=p.first;
+            else {
+                if (start!=-1) {
+                    res.push_back(Interval(start, p.first));
+                    start=-1;
+                }
             }
-            prevCnt=cnt;
         }
+
         return res;
     }
 };
