a89e919886b8552364b33645af7ed677bf8c65a0
jtliames2013 <jtli.ames2013@gmail.com>
2020-01-01 16:26:34 -0800

    more checkin

---
+++ cpp/confusing-number-ii.cpp
@@ -0,0 +1,71 @@
+1088. Confusing Number II
+Hard
+
+58
+
+24
+
+Add to List
+
+Share
+We can rotate digits by 180 degrees to form new digits. When 0, 1, 6, 8, 9 are rotated 180 degrees, they become 0, 1, 9, 8, 6 respectively. When 2, 3, 4, 5 and 7 are rotated 180 degrees, they become invalid.
+
+A confusing number is a number that when rotated 180 degrees becomes a different number with each digit valid.(Note that the rotated number can be greater than the original number.)
+
+Given a positive integer N, return the number of confusing numbers between 1 and N inclusive.
+
+ 
+
+Example 1:
+
+Input: 20
+Output: 6
+Explanation: 
+The confusing numbers are [6,9,10,16,18,19].
+6 converts to 9.
+9 converts to 6.
+10 converts to 01 which is just 1.
+16 converts to 91.
+18 converts to 81.
+19 converts to 61.
+Example 2:
+
+Input: 100
+Output: 19
+Explanation: 
+The confusing numbers are [6,9,10,16,18,19,60,61,66,68,80,81,86,89,90,91,98,99,100].
+ 
+
+Note:
+
+1 <= N <= 10^9
+
+Google
+|
+9
+
+class Solution {
+public:
+    int confusingNumberII(int N) {
+        count=0;
+        upper=N;
+        if (N>=1) dfs(1, 1, 10);
+        if (N>=6) dfs(6, 9, 10);
+        if (N>=8) dfs(8, 8, 10);
+        if (N>=9) dfs(9, 6, 10);
+        return count;
+    }
+private:
+    void dfs(long num, long rotation, long base) {
+        if (num!=rotation) count++;
+        for (auto d:digits) {
+            if (num*10+d<=upper) dfs(num*10+d, mp[d]*base+rotation, base*10);
+        }
+    }
+    
+    int count;
+    int upper;
+    vector<int> digits={0, 1, 8, 6, 9};
+    unordered_map<int, int> mp={{0,0},{1,1},{8,8},{6,9},{9,6}};
+};
+
