bd77eb8a812fe19805f1c9a949de59f28523fa32
jtliames2013 <jtli.ames2013@gmail.com>
2020-01-03 10:16:51 -0800

    more checkin

--- cpp/rearrange-string-k-distance-apart.cpp
+++ cpp/rearrange-string-k-distance-apart.cpp
@@ -1,33 +1,44 @@
-358. Rearrange String k Distance Apart 
-Given a non-empty string str and an integer k, rearrange the string such that the same characters are at least distance k from each other.
+358. Rearrange String k Distance Apart
+Hard
+
+356
+
+16
+
+Add to List
+
+Share
+Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
 
 All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string "".
 
 Example 1:
-str = "aabbcc", k = 3
 
-Result: "abcabc"
-
-The same letters are at least distance 3 from each other.
+Input: s = "aabbcc", k = 3
+Output: "abcabc" 
+Explanation: The same letters are at least distance 3 from each other.
 Example 2:
-str = "aaabc", k = 3 
 
-Answer: ""
-
-It is not possible to rearrange the string.
+Input: s = "aaabc", k = 3
+Output: "" 
+Explanation: It is not possible to rearrange the string.
 Example 3:
-str = "aaadbbcc", k = 2
 
-Answer: "abacabcd"
+Input: s = "aaadbbcc", k = 2
+Output: "abacabcd"
+Explanation: The same letters are at least distance 2 from each other.
 
-Another possible answer is: "abcabcda"
+Facebook
+|
+2
 
-The same letters are at least distance 2 from each other.
-Credits:
-Special thanks to @elmirap for adding this problem and creating all test cases.
+Amazon
+|
+2
 
-Hide Company Tags Google
-Hide Tags Hash Table Heap Greedy
+Google
+|
+5
 
 The greedy algorithm is that in each step, select the char with highest remaining count if possible.
 
