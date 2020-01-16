ecdcdaf6d8b9cbe4de8e084808a42b8882520726
jtliames2013 <jtli.ames2013@gmail.com>
2020-01-15 19:59:29 -0800

    more checkin

--- cpp/4sum.cpp
+++ cpp/4sum.cpp
@@ -88,28 +88,26 @@ public:
         if (n<4) return res;
         sort(nums.begin(), nums.end());
         for (int i=0; i<n-3; i++) {
-            if (i==0 || nums[i]!=nums[i-1]) {
-                if (nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
-                if (nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;
-                for (int j=i+1; j<n-2; j++) {
-                    if (j==i+1 || nums[j]!=nums[j-1]) {
-                        if (nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
-                        if (nums[i]+nums[j]+nums[n-2]+nums[n-1]<target) continue;
-                        int left=j+1, right=n-1;
-                        while (left<right) {
-                            int sum=nums[left]+nums[right]+nums[i]+nums[j];
-                            if (sum==target) {
-                                res.push_back({nums[i],nums[j],nums[left],nums[right]});
-                                left++;
-                                right--;
-                                while (left<right && nums[left]==nums[left-1]) left++;
-                                while (left<right && nums[right]==nums[right+1]) right--;
-                            } else if (sum>target) {
-                                right--;
-                            } else {
-                                left++;
-                            }
-                        }
+            if (nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
+            if (nums[i]+nums[n-3]+nums[n-2]+nums[n-1]<target) continue;
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            for (int j=i+1; j<n-2; j++) {
+                if (nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
+                if (nums[i]+nums[j]+nums[n-2]+nums[n-1]<target) continue;
+                if (j>i+1 && nums[j]==nums[j-1]) continue;
+                int left=j+1, right=n-1;
+                while (left<right) {
+                    int sum=nums[left]+nums[right]+nums[i]+nums[j];
+                    if (sum==target) {
+                        res.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+                        while (left<right && nums[left]==nums[left-1]) left++;
+                        while (left<right && nums[right]==nums[right+1]) right--;
+                    } else if (sum>target) {
+                        right--;
+                    } else {
+                        left++;
                     }
                 }
             }
