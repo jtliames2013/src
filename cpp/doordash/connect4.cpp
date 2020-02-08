fac423f862a007bbaa8f32486ce85d3b5cdcc1d0
jtliames2013 <jtli.ames2013@gmail.com>
2020-02-08 13:28:43 -0800

    more checkin

---
+++ cpp/doordash/connect4.cpp
@@ -0,0 +1,109 @@
+https://en.wikipedia.org/wiki/Connect_Four
+
+class Connect4 {
+public:
+    Connect4() {
+        rowIndex.resize(numCols, numRows-1);
+        mat.resize(numRows, vector<int>(numCols));
+    }
+
+    int move(int col, int player) {
+        if (col<0 || col>=numCols || rowIndex[col]==0) return -1;
+        int row=rowIndex[col];
+        mat[row][col]=player;
+        rowIndex[col]--;
+        print();
+
+        if (check(row, col, player)) return player;                
+        return 0;
+    }
+
+    bool check(int row, int col, int player) {        
+        vector<vector<int>> delta;
+        // check row
+        delta={{0, -1}, {0, 1}};
+        if (checkOne(row, col, player, delta)) return true;
+
+        // check col
+        delta={{-1, 0}, {1, 0}};
+        if (checkOne(row, col, player, delta)) return true;
+
+        // check diag
+        delta={{-1, -1}, {1, 1}};
+        if (checkOne(row, col, player, delta)) return true;
+
+        // check antidiag
+        delta={{-1, 1}, {1, -1}};
+        if (checkOne(row, col, player, delta)) return true;
+
+        return false;
+    }
+
+    bool checkOne(int row, int col, int player, vector<vector<int>>& delta) {
+        int count=1;
+        for (int k=0; k<2; ++k) {
+            int nr=row, nc=col;
+            for (int i=0; i<num; ++i) {
+                nr+=delta[k][0];
+                nc+=delta[k][1];
+                if (nr<0 || nr>=numRows || nc<0 || nc>=numCols) break;
+                if (mat[nr][nc]!=player) break;
+                count++;
+                if (count==num) return true;
+            }            
+        }
+        return false;
+    }
+
+    int getInput(int player) {
+        int col;
+        cout << "Please input the column for player " << player << ": " << endl;
+        cin >> col;
+        return col;
+    }
+
+    void print() {
+        for (int i=0; i<numCols; ++i) {
+            if (i>0) cout << ", ";
+            cout << rowIndex[i];
+        }
+        cout << endl << endl;
+
+        for (int i=0; i<numRows; ++i) {
+            for (int j=0; j<numCols; ++j) {
+                if (j>0) cout << ", ";
+                cout << mat[i][j];
+            }
+            cout << endl;        
+        }
+        cout << endl;
+    }
+private:
+    const int numRows=6;
+    const int numCols=7;
+    const int num=4;
+    vector<int> rowIndex;
+    vector<vector<int>> mat;
+};
+
+int main(int argc, char** argv) {
+    Connect4 c;
+    int player=1;
+    while (1) {
+        int col=c.getInput(player);
+
+        int res=c.move(col, player);
+        if (res>0) {
+            cout << "Player "<< res << " wins!" << endl;
+            break;
+        } else if (res<0) {
+            cout << "Invalid input!" << endl;
+            break;
+        }
+
+        player=player==1?2:1;
+    }
+
+    return 0;
+}
+
