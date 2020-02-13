d0be60e5ec2bfa6da61cc78d458feefe0448bdb6
jtliames2013 <jtli.ames2013@gmail.com>
2020-02-13 07:20:07 -0800

    more checkin

--- cpp/minimum-path-sum.cpp
+++ cpp/minimum-path-sum.cpp
@@ -34,7 +34,24 @@ public:
     }
 };
 
-2. Use 1D dp array
+2.
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size(), n=grid[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        for (int i=1; i<=m; ++i) {
+            for (int j=1; j<=n; ++j) {                
+                dp[i][j]=min(dp[i-1][j], dp[i][j-1]);
+                if (dp[i][j]==INT_MAX) dp[i][j]=0;
+                dp[i][j]+=grid[i-1][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+3. Use 1D dp array
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
