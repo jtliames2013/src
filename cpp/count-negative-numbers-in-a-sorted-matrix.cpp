3912a43d00ed3de85d83191296fb5bcd269bb9ac
jtliames2013 <jtli.ames2013@gmail.com>
2020-02-15 20:35:05 -0800

    more checkin

---
+++ cpp/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,58 @@
+1351. Count Negative Numbers in a Sorted Matrix
+Easy
+
+8
+
+1
+
+Add to List
+
+Share
+Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise. 
+
+Return the number of negative numbers in grid.
+
+ 
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+Example 3:
+
+Input: grid = [[1,-1],[-1,-1]]
+Output: 3
+Example 4:
+
+Input: grid = [[-1]]
+Output: 1
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 100
+-100 <= grid[i][j] <= 100
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int res=0, m=grid.size(), n=grid[0].size();
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (grid[i][j]<0) {
+                    res+=n-j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
