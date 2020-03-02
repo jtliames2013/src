c1e4432388c6ca96e2c0f16aae2a3d07fed01345
jtliames2013 <jtli.ames2013@gmail.com>
2020-03-02 09:48:01 -0800

    more checkin

--- cpp/divide-two-integers.cpp
+++ cpp/divide-two-integers.cpp
@@ -1,21 +1,43 @@
-29. Divide Two Integers  
-Divide two integers without using multiplication, division and mod operator.
+29. Divide Two Integers
+Medium
 
-If it is overflow, return MAX_INT.
+934
+
+4564
+
+Add to List
+
+Share
+Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero.
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Note:
+
+Both dividend and divisor will be 32-bit signed integers.
+The divisor will never be 0.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
 
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (divisor==0) {
-            if (dividend==0) return INT_MAX;
-            else return INT_MAX;
-        }
+        if (divisor==0) return INT_MAX;
         if (dividend==INT_MIN && divisor==-1) return INT_MAX;
         long res=0;
         bool neg=(dividend<0 ^ divisor<0);
         long dd=abs((long)dividend);
         long dr=abs((long)divisor);
-        
+
         while (dd>=dr) {
             int i=0;
             while (dd>=(dr<<i)) i++;
@@ -26,3 +48,4 @@ public:
         return neg?-res:res;
     }
 };
+
