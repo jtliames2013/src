f1edc3d9ba619306a9ebf8e672a2485936b2ad81
jtliames2013 <jtli.ames2013@gmail.com>
2020-03-30 23:10:03 -0700

    more checkin

--- cpp/find-all-good-strings.cpp
+++ cpp/find-all-good-strings.cpp
@@ -38,5 +38,47 @@ s2.length == n
 1 <= evil.length <= 50
 All strings consist of lowercase English letters.
 
-
+class Solution {
+public:
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+        vector<int> memo(1<<17, -1);
+        vector<int> p=precompute(evil);
+        return dfs(s1, s2, evil, n, memo, p, 0, 0, 1, 1);
+    }
+private:
+    int dfs(string& s1, string& s2, string& evil, int n, vector<int>& memo, vector<int>& p, int sIdx, int eIdx, int leftBound, int rightBound) {
+        if (eIdx==evil.size()) return 0;
+        if (sIdx==n) return 1;
+        int key=getKey(sIdx, eIdx, leftBound, rightBound);
+        if (memo[key]!=-1) return memo[key];
+        int res=0;
+        char from=leftBound?s1[sIdx]:'a';
+        char to=rightBound?s2[sIdx]:'z';
+        for (char c=from; c<=to; ++c) {
+            int j=eIdx;
+            while (j>0 && evil[j]!=c) j=p[j-1];
+            if (c==evil[j]) j++;
+            res+=dfs(s1, s2, evil, n, memo, p, sIdx+1, j, leftBound && (c==from), rightBound && (c==to));
+            res%=mod;
+        }
+        
+        memo[key]=res;
+        return memo[key];
+    }
+    
+    int getKey(int i, int j, int l, int r) {
+        return (i<<8) | (j<<2) | (l<<1) | r;
+    }
+    
+    vector<int> precompute(string& s) {
+        int n=s.size();
+        vector<int> res(n);
+        for (int i=1, j=0; i<n; ++i) {
+            while (j>0 && s[i]!=s[j]) j=res[j-1];
+            if (s[i]==s[j]) res[i]=++j;
+        }
+        return res;
+    }
+    const int mod=1e9+7;
+};
 
