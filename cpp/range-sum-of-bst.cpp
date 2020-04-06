2b29478961f66b53539e3056b9319fc8ebe1916d
jtliames2013 <jtli.ames2013@gmail.com>
2020-04-05 18:59:49 -0700

    more checkin

--- cpp/range-sum-of-bst.cpp
+++ cpp/range-sum-of-bst.cpp
@@ -47,3 +47,14 @@ public:
         return res+rangeSumBST(root->left, L, R)+rangeSumBST(root->right, L, R);        
     }
 };
+
+2.
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if (root==NULL) return 0;
+        if (root->val<L) return rangeSumBST(root->right, L, R);
+        if (root->val>R) return rangeSumBST(root->left, L, R);
+        return root->val+rangeSumBST(root->left, L, R)+rangeSumBST(root->right, L, R);
+    }
+};
