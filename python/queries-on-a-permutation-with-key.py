ec437e481fa163a5db2a2e7cf3d662ce56f694fe
jtliames2013 <jtli.ames2013@gmail.com>
2020-04-12 17:41:04 -0700

    more checkin

---
+++ python/queries-on-a-permutation-with-key.py
@@ -0,0 +1,11 @@
+class Solution:
+    def processQueries(self, queries: List[int], m: int) -> List[int]:
+        res=[]
+        P=list(range(1, m+1))
+        for q in queries:
+            i=P.index(q)
+            res.append(i)
+            P=[q]+P[:i]+P[i+1:]
+            
+        return res
+
