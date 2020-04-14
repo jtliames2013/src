aaa3a9ddb32e1bdec5d4d7b513640cd024470a55
jtliames2013 <jtli.ames2013@gmail.com>
2020-04-13 21:29:57 -0700

    more checkin

---
+++ python/split-a-string-in-balanced-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res, cnt=0, 0
+        for c in s:
+            cnt+=1 if c=='L' else -1
+            if cnt==0: res+=1
+        
+        return res
+
