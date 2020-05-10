50a2910fee5dd92a09a8c2da9f0da33c82a4b6b5
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-09 23:11:21 -0700

    more checkin

---
+++ python/build-an-array-with-stack-operations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res=[]
+        j=0
+        for i in range(1, n+1):
+            if j>=len(target): break
+            if i==target[j]:
+                res.append("Push")
+                j+=1
+            else:
+                res.append("Push")
+                res.append("Pop")
+        
+        return res
+
