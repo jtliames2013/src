6a7280002313ab1be39a6c754ebab8af5fb65d33
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-25 10:28:28 -0700

    more checkin

--- python/merge-k-sorted-lists.py
+++ python/merge-k-sorted-lists.py
@@ -1,25 +1,44 @@
 # Definition for singly-linked list.
 # class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 from Queue import PriorityQueue
-class Solution:
+class Solution(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        dummy=ListNode(None)
-        tail=dummy
+        dummy=tail=ListNode()
         pq=PriorityQueue()
         for l in lists:
             if l: pq.put((l.val, l))
         while not pq.empty():
             t=pq.get()[1]
             tail.next=t
-            tail=tail.next
+            tail=t
             if t.next: pq.put((t.next.val, t.next))
+        return dummy.next        
+
+
+2. heapq
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy=tail=ListNode()
+        # in case of tie-break of val comparison, compare index instead of ListNode
+        pq=[(l.val, i, l) for i, l in enumerate(lists) if l]
+        heapify(pq)
+        while pq:
+            v, i, l=pq[0]
+            heappop(pq)
+            tail.next=l
+            tail=l
+            if l.next: heappush(pq, (l.next.val, i, l.next))
         return dummy.next
 
