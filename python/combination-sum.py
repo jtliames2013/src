dfb68a81bfda966eed1df6c7191fdf62c8758749
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-25 14:33:09 -0700

    more checkin

--- python/combination-sum.py
+++ python/combination-sum.py
@@ -1,22 +1,17 @@
 class Solution:
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        res=[]
-        output=[]
-        self.getSum(res, output, candidates, target, 0)
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(output, candidates, target, start):
+            if target==0: 
+                res.append(output[:])
+                return
+            
+            for i in range(start, len(candidates)):
+                if target>=candidates[i]:
+                    output.append(candidates[i])
+                    dfs(output, candidates, target-candidates[i], i)
+                    output.pop()
+                    
+        res, output=[], []
+        dfs(output, candidates, target, 0)
         return res
-    
-    def getSum(self, res, output, candidates, target, start):
-        if target==0:
-            res.append(list(output))
-            return
-        
-        for i in range(start, len(candidates)):
-            if target-candidates[i]>=0:
-                output.append(candidates[i])                
-                self.getSum(res, output, candidates, target-candidates[i], i)
-                output.pop()                
+
