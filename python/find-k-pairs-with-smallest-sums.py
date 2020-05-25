ed4020fde43937a3d4148db10b8eae4665be7737
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-25 11:15:19 -0700

    more checkin

--- python/find-k-pairs-with-smallest-sums.py
+++ python/find-k-pairs-with-smallest-sums.py
@@ -1,25 +1,13 @@
 class Solution:
-    def kSmallestPairs(self, nums1, nums2, k):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :type k: int
-        :rtype: List[List[int]]
-        """
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         res=[]
-        if len(nums1)==0 or len(nums2)==0: return res
-        
-        h=[]
-        heapq.heappush(h, [nums1[0]+nums2[0], 0, 0])
-        
-        while len(h)>0 and k>0:
-            _, i, j=heapq.heappop(h)
+        if not nums1 or not nums2: return res
+        pq=[[nums1[0]+nums2[0], 0, 0]]
+        while pq and k>0:
+            _, i, j=heappop(pq)
             res.append([nums1[i], nums2[j]])
-            if i<len(nums1)-1:
-                heapq.heappush(h, [nums1[i+1]+nums2[j], i+1, j])
-            if i==0 and j<len(nums2)-1:
-                heapq.heappush(h, [nums1[i]+nums2[j+1], i, j+1])
+            if i==0 and j<len(nums2)-1: heappush(pq, [nums1[i]+nums2[j+1], i, j+1])
+            if i<len(nums1)-1: heappush(pq, [nums1[i+1]+nums2[j], i+1, j])
             k-=1
-            
         return res
-        
+
