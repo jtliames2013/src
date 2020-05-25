9a986b39d52fe9809f298c3d607d691c134dc2bf
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-25 11:58:28 -0700

    more checkin

--- python/k-closest-points-to-origin.py
+++ python/k-closest-points-to-origin.py
@@ -6,3 +6,19 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         return heapq.nsmallest(K, points, lambda (x, y): x*x+y*y)
+
+2.
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        res, pq=[], []
+        for p in points:
+            d=p[0]*p[0]+p[1]*p[1]
+            if len(pq)<K: heappush(pq, [-d, p[0], p[1]])
+            elif d<-pq[0][0]:
+                heappop(pq)
+                heappush(pq, [-d, p[0], p[1]])
+        while pq:
+            d, i, j=heappop(pq)
+            res.append([i, j])
+        return res
+
