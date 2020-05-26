7e20f3b79ed2cb542f3e8dba3ded68e51c064555
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-26 15:09:36 -0700

    more checkin

--- python/palindrome-number.py
+++ python/palindrome-number.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x < 0:
-            return False;
-        base = 1
-        while base <= x:
-            base *= 10
-        base /=10
-        while x > 0:
-            if x % 10 != x / base:
-                return False
-            x %= base
-            x /= 10
-            base /= 100
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0: return False
+        base=1
+        while base<=x: base*=10
+        base//=10
+        while x>0:
+            if x//base != x%10: return False
+            x%=base
+            x//=10
+            base//=100
         return True
+
