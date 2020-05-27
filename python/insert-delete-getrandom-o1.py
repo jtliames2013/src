09b67727525365f19e064efba463908ba80b5832
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-26 18:05:21 -0700

    more checkin

--- python/insert-delete-getrandom-o1.py
+++ python/insert-delete-getrandom-o1.py
@@ -1,41 +1,36 @@
-class RandomizedSet(object):
-
+class RandomizedSet:
+    
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.data, self.mp=[], {}
+        self.data, self.dic=[], {}
 
-    def insert(self, val):
+    def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        :type val: int
-        :rtype: bool
         """
-        if val in self.mp: return False
-        self.mp[val]=len(self.data)
+        if val in self.dic: return False
+        self.dic[val]=len(self.data)
         self.data.append(val)
         return True
 
-    def remove(self, val):
+    def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
-        :type val: int
-        :rtype: bool
         """
-        if val not in self.mp: return False
-        if val != self.data[-1]:
-            idx=self.mp[val]
-            self.mp[self.data[-1]]=idx
+        if val not in self.dic: return False
+        if val!=self.data[-1]:
+            idx=self.dic[val]
+            self.dic[self.data[-1]]=idx
             self.data[idx]=self.data[-1]
         self.data.pop()
-        self.mp.pop(val)
+        self.dic.pop(val, None)
         return True
 
-    def getRandom(self):
+    def getRandom(self) -> int:
         """
         Get a random element from the set.
-        :rtype: int
         """
         return self.data[random.randint(0, len(self.data)-1)]
 
@@ -45,3 +40,4 @@ class RandomizedSet(object):
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
