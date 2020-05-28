3f307413acf494b0c693d2acc9be4bc7016e88d4
jtliames2013 <jtli.ames2013@gmail.com>
2020-05-27 17:48:23 -0700

    more checkin

--- python/valid-palindrome-ii.py
+++ python/valid-palindrome-ii.py
@@ -1,3 +1,4 @@
+1.
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         l, r=0, len(s)-1
@@ -7,4 +8,17 @@ class Solution:
                 return one==one[::-1] or two==two[::-1]
             l, r=l+1, r-1
         return True
-        
+  
+2.
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def dfs(s, l, r, delete):
+            while l<r:
+                if s[l]!=s[r]:
+                    if delete==0: return False
+                    return dfs(s, l+1, r, 0) or dfs(s, l, r-1, 0)
+                else:
+                    l, r=l+1, r-1
+            return True
+        return dfs(s, 0, len(s)-1, 1)
+
