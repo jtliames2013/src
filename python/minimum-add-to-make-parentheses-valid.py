d99712550c937133628b62e3e6ac0df71744aa09
jtliames2013 <jtli.ames2013@gmail.com>
2020-06-06 18:29:14 -0700

    more checkin

---
+++ python/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minAddToMakeValid(self, S: str) -> int:
+        left, right=0, 0
+        for c in S:
+            if c=='(': left+=1
+            elif left>0: left-=1
+            else: right+=1
+        return left+right
+
