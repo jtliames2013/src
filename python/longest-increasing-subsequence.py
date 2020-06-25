4b7cc7680e0d2bf226e9f8a0ee0e41327c16a50b
jtliames2013 <jtli.ames2013@gmail.com>
2020-06-25 08:29:31 -0700

    more checkin

--- python/longest-increasing-subsequence.py
+++ python/longest-increasing-subsequence.py
@@ -1,3 +1,16 @@
+1.
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums: return 0
+        res, n=float('-inf'), len(nums)
+        dp=[1]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[i]>nums[j]: dp[i]=max(dp[i], dp[j]+1)
+            res=max(res, dp[i])
+        return res
+
+2.
 class Solution:
     def lengthOfLIS(self, nums):
         """
