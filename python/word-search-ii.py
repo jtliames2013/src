73dcd7216c1963024cf6f5e57c75cf7fbff73450
jtliames2013 <jtli.ames2013@gmail.com>
2020-07-28 13:49:34 -0700

    more checkin

---
+++ python/word-search-ii.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children=collections.defaultdict(TrieNode)
+        self.word=''
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def dfs(row, col, node):
+            if row<0 or row>=m or col<0 or col>=n: return
+            c=board[row][col]            
+            if c=='#'  or c not in node.children: return
+            node=node.children[c]
+            if node.word:
+                res.append(node.word)
+                node.word=''
+            
+            board[row][col]='#'
+            dfs(row+1, col, node)
+            dfs(row-1, col, node)
+            dfs(row, col+1, node)
+            dfs(row, col-1, node)
+            board[row][col]=c
+            
+        res=[]
+        m=len(board)
+        if m==0: return res
+        n=len(board[0])
+        if n==0: return res
+        root=TrieNode()
+        for w in words:
+            node=root
+            for c in w:
+                node=node.children[c]
+            node.word=w
+        
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+        
+        return res
+
