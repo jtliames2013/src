af57260dfb82500ee3fd26ff61820e55764a5cd2
jtliames2013 <jtli.ames2013@gmail.com>
2020-08-04 14:15:25 -0700

    more checkin

--- python/validate-binary-search-tree.py
+++ python/validate-binary-search-tree.py
@@ -1,3 +1,4 @@
+1. recursive
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,3 +14,30 @@ class Solution:
         
         return dfs(root, float('-inf'), float('inf'))
 
+2. iterative
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        prev=float('-inf')
+        stk=[]
+        if not root: return True
+        n=root
+        while n:
+            stk.append(n)
+            n=n.left
+
+        while stk:
+            t=stk.pop()
+            if t.val<=prev: return False
+            prev=t.val
+
+            n=t.right
+            while n:
+                stk.append(n)
+                n=n.left
+        return True
