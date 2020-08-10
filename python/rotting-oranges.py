c1735eeab7b429ac38d8971a80447d2b23447a2d
jtliames2013 <jtli.ames2013@gmail.com>
2020-08-09 22:17:27 -0700

    more checkin

--- python/rotting-oranges.py
+++ python/rotting-oranges.py
@@ -1,25 +1,23 @@
-import queue
-
 class Solution:
     def orangesRotting(self, grid: 'List[List[int]]') -> 'int':
         m, n=len(grid), len(grid[0])
-        q=q = queue.Queue()
+        q=[]
         count, minutes=0, -1
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==2: q.put([i,j])
+                if grid[i][j]==2: q.append([i,j])
                 elif grid[i][j]==1: count+=1
         if count==0: return 0
         
-        while not q.empty():
-            size=q.qsize()
+        while q:
+            size=len(q)
             for i in range(size):
-                f=q.get()
+                f=q.pop(0)
                 for nr, nc in [(f[0]-1, f[1]), (f[0],f[1]-1), (f[0]+1, f[1]), (f[0], f[1]+1)]:
                     if nr>=0 and nr<m and nc>=0 and nc<n and grid[nr][nc]==1:
                         grid[nr][nc]=2
                         count-=1
-                        q.put([nr,nc])
+                        q.append([nr,nc])
             minutes+=1
             
         return minutes if count==0 else -1
