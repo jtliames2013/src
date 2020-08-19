e98a18e97268e1be512afcada166f0c06d733eca
jtliames2013 <jtli.ames2013@gmail.com>
2020-08-18 23:05:11 -0700

    more checkin

--- python/time-based-key-value-store.py
+++ python/time-based-key-value-store.py
@@ -20,3 +20,24 @@ class TimeMap:
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
 
+2.
+class TimeMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dic=collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dic[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dic: return ""
+        l, r=0, len(self.dic[key])
+        while l<r:
+            mid=(l+r)//2
+            if self.dic[key][mid][0]<=timestamp: l=mid+1
+            else: r=mid
+        return '' if l==0 else self.dic[key][l-1][1]
+
