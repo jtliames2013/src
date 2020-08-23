96434d8190c0b1520025f45e7f69b89c0fe55db4
jtliames2013 <jtli.ames2013@gmail.com>
2020-08-22 21:52:05 -0700

    more checkin

---
+++ python/most-visited-sector-in-a-circular-track.py
@@ -0,0 +1,4 @@
+class Solution:
+    def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
+        return range(rounds[0], rounds[-1]+1) or list(range(1, rounds[-1]+1))+list(range(rounds[0], n+1))
+
