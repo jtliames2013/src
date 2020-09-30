bc6ff2ff941d1b13d9700ab3d1e6d67de1059b5c
jtliames2013 <jtli.ames2013@gmail.com>
2020-09-30 10:32:52 -0700

    more checkin

--- cpp/sqrtx.cpp
+++ cpp/sqrtx.cpp
@@ -30,21 +30,3 @@ public:
     }
 };
 
-2.
-class Solution {
-public:
-    int mySqrt(int x) {
-        if (x==0) return 0;
-        int l=1, r=x, mid;
-        
-        while (l<=r) {
-            mid=l+(r-l)/2;    
-            if (mid==x/mid) return mid;
-            else if (mid<x/mid) l=mid+1;
-            else r=mid-1;
-        }
-        
-        return r;
-    }
-};
-
--- python/sqrtx.py
+++ python/sqrtx.py
@@ -1,33 +1,9 @@
-1.
 class Solution:
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """        
-        l, r=0, x
-        while l<=r:
-            mid=int(l+(r-l)/2)
-            s=mid*mid
-            if s==x: return mid
-            elif s<x: l=mid+1
-            else: r=mid-1
-        
-        return r
-
-2.
-class Solution:
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """        
+    def mySqrt(self, x: int) -> int:
         l, r=0, x
         while l<r:
-            mid=int(l+(r-l)/2+1)
-            s=mid*mid
-            if s==x: return mid
-            elif s<x: l=mid
-            else: r=mid-1
-        
-        return r
+            mid=(l+r)//2+1
+            if mid*mid>x: r=mid-1
+            else: l=mid
+        return l
+
