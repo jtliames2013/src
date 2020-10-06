9376da23db60e20a6c1c72b5ed48eee247a27f13
jtliames2013 <jtli.ames2013@gmail.com>
2020-10-05 23:21:36 -0700

    more checkin

--- python/top-k-frequent-elements.py
+++ python/top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+1.
 class Solution:
     def topKFrequent(self, nums, k):
         """
@@ -6,3 +7,19 @@ class Solution:
         :rtype: List[int]
         """
         return list(zip(*collections.Counter(nums).most_common(k)))[0]
+
+2.
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count=collections.Counter(nums)
+        pq=[]
+        for i, c in count.items():
+            if len(pq)<k: heappush(pq, [c, i])
+            elif c>pq[0][0]:
+                heappop(pq)
+                heappush(pq, [c, i])
+        res=[]
+        while pq:
+            res.append(pq.pop()[1])
+        return res
+
