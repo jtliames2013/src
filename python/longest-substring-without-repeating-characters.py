794910a45063cd170676a9dc571cdb28dd2eb1de
jtliames2013 <jtli.ames2013@gmail.com>
2020-10-07 21:09:02 -0700

    more checkin

--- python/longest-substring-without-repeating-characters.py
+++ python/longest-substring-without-repeating-characters.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pos = {}
-        longest = 0
-        if len(s) == 0:
-            return longest
-        l = 0
-        r = 0
-        for r in range(len(s)):
-            if s[r] in pos:
-                longest = max(longest, r - l)
-                for i in range(l, pos[s[r]]):
-                    del pos[s[i]]
-                l = pos[s[r]] + 1
-            pos[s[r]] = r
-        longest = max(longest, r - l + 1)
-        return longest
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic=collections.defaultdict(int)
+        res, l, r, n=0, 0, 0, len(s)
+        while r<n:
+            if s[r] in dic:
+                res=max(res, r-l)
+                while l<=dic[s[r]]:
+                    dic.pop(s[l])
+                    l+=1
+            dic[s[r]]=r
+            r+=1
+        res=max(res, r-l)
+        return res
+
