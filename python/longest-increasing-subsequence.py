8189b199d349227b2249ef5edb55dfd3aaaeeb7f
jtliames2013 <jtli.ames2013@gmail.com>
2020-10-11 16:29:29 -0700

    more checkin

--- python/longest-increasing-subsequence.py
+++ python/longest-increasing-subsequence.py
@@ -12,19 +12,26 @@ class Solution:
 
 2.
 class Solution:
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def lengthOfLIS(self, nums: List[int]) -> int:
         sorted=[]
         for num in nums:
             l, r=0, len(sorted)
             while l<r:
-                mid=int(l+(r-l)/2)
+                mid=(l+r)//2
                 if sorted[mid]<num: l=mid+1
                 else: r=mid
             if l==len(sorted): sorted.append(num)
             else: sorted[l]=num
-        
+
         return len(sorted)
+
+3.
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sorted=[]
+        for n in nums:
+            pos=bisect.bisect_left(sorted, n)
+            if pos==len(sorted): sorted.append(n)
+            else: sorted[pos]=n
+        return len(sorted)
+
