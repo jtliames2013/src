c9df8a47a8c24327889e93e0604ecb2925640e3f
jtliames2013 <jtli.ames2013@gmail.com>
2020-10-11 16:12:59 -0700

    more checkin

--- python/decode-string.py
+++ python/decode-string.py
@@ -1,3 +1,4 @@
+1.
 class Solution:
     def decodeString(self, s: str) -> str:
         res, num, stk='', '', []
@@ -12,5 +13,30 @@ class Solution:
                 res=st+res*n
             else:
                 res+=c
+        return res
+
+2.
+class Solution:
+    def decodeString(self, s: str) -> str:
+        res, stk, i, n='', [], 0, len(s)
+        while i<n:
+            if s[i].isdigit():
+                num=0
+                j=i
+                while j<n and s[j].isdigit():
+                    num=num*10+ord(s[j])-ord('0')
+                    j+=1
+                i=j
+            elif s[i]=='[':
+                stk.append([res, num])
+                res=''
+                i+=1
+            elif s[i]==']':
+                st, num=stk.pop()
+                res=st+res*num
+                i+=1
+            else:
+                res+=s[i]
+                i+=1
         return res
 
