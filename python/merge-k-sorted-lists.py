31b4ad1045565bdec9e9f484b1a56e758c152d12
jtliames2013 <jtli.ames2013@gmail.com>
2020-10-18 10:22:16 -0700

    more checkin

--- python/merge-k-sorted-lists.py
+++ python/merge-k-sorted-lists.py
@@ -23,17 +23,29 @@ class Solution(object):
 
 
 2. heapq
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         dummy=tail=ListNode()
         # in case of tie-break of val comparison, compare index instead of ListNode
         pq=[(l.val, i, l) for i, l in enumerate(lists) if l]
         heapify(pq)
+        while pq:
+            v, i, l=pq[0]
+            heappop(pq)
+            tail.next=l
+            tail=l
+            if l.next: heappush(pq, (l.next.val, i, l.next))
+        return dummy.next
+
+3.
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy=tail=ListNode()
+        # in case of tie-break of val comparison, compare index instead of ListNode
+        pq=[]
+        for i, l in enumerate(lists):
+            if l:
+                heappush(pq, (l.val, i, l))
         while pq:
             v, i, l=pq[0]
             heappop(pq)
