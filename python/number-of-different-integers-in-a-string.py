54a1f319e5c81f31f31d69650a198d3c2bed9b3a
jtliames2013 <jtli.ames2013@gmail.com>
2021-03-28 08:32:38 -0700

    more checkin

--- python/number-of-different-integers-in-a-string.py
+++ python/number-of-different-integers-in-a-string.py
@@ -1,14 +1,5 @@
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        st=set()
-        i, n=0, len(word)
-        while i<n:
-            if word[i].isdigit():
-                j=i
-                while j<n and word[j].isdigit(): j+=1
-                st.add(int(word[i:j])) 
-                i=j
-            else:
-                i+=1
-        return len(st)
+        s=''.join(c if c.isdigit() else ' ' for c in word)
+        return len(collections.Counter(map(int, s.split())))
 
