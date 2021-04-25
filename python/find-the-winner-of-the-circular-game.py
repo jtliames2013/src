8203645bfc93b412c683f12e6e97793fafdcbc47
jtliames2013 <jtli.ames2013@gmail.com>
2021-04-24 22:30:56 -0700

    more checkin

--- python/find-the-winner-of-the-circular-game.py
+++ python/find-the-winner-of-the-circular-game.py
@@ -1,7 +1,18 @@
+1.
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         res=0
         for i in range(1, n+1):
             res=(res+k)%i
         return res+1
+
+2.
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        nums=list(range(n))
+        i=0
+        while len(nums)>1:
+            i=(i+k-1)%len(nums)
+            nums.pop(i)
+        return nums[0]+1
 
