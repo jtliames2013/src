adca8fb7f3d67d6f08717a5019b9a7db9cd172f0
jtliames2013 <jtli.ames2013@gmail.com>
2021-05-01 17:52:27 -0700

    more checkin

--- cpp/replace-all-digits-with-characters.cpp
+++ cpp/replace-all-digits-with-characters.cpp
@@ -48,10 +48,8 @@ class Solution {
 public:
     string replaceDigits(string s) {
         int n=s.size();
-        for (int i=0; i<n; ++i) {
-            if (i%2==1) {
-                s[i]=s[i-1]+s[i]-'0';
-            }
+        for (int i=1; i<n; i+=2) {
+            s[i]=s[i-1]+s[i]-'0';
         }
         return s;
     }
